8580237e2c6d86da164560825bec932e398b7d4d
greg_honda <honda.gregory@epa.gov>
2019-06-05 16:29:00 -0400

    Updated to work off Parameterize_SteadyState

--- R/calc_oral_bioavailability.R
+++ R/calc_oral_bioavailability.R
@@ -64,33 +64,41 @@ calc_fbio.oral <- function(Params = NULL,
                            fup.lod.default=0.005,
                            suppress.messages=F,
                            minimum.Funbound.plasma=0.0001,
-                           Caco2.Pab.default = 1
+                           Caco2.options = list(Caco2.Pab.default = 2,
+                                                Caco2.Fgut = TRUE,
+                                                Caco2.Fabs = TRUE)
 ){
   # Header initialization  
   if(is.null(Params)){
     out <- get_chem_id(chem.cas=chem.cas,chem.name=chem.name)
     chem.cas <- out$chem.cas
     chem.name <- out$chem.name
-    Params <- parameterize_fbio(chem.cas = chem.cas,
-                                     chem.name = chem.name,
-                                     species = species,
-                                     clint.pvalue.threshold = clint.pvalue.threshold,
-                                     default.to.human = default.to.human,
-                                     human.clint.fup = human.clint.fup,
-                                     adjusted.Funbound.plasma = adjusted.Funbound.plasma,
-                                     restrictive.clearance = restrictive.clearance,
-                                     fup.lod.default = fup.lod.default,
-                                     suppress.messages = suppress.messages,
-                                     minimum.Funbound.plasma = minimum.Funbound.plasma,
-                                Caco2.Pab.default = Caco2.Pab.default)
+    Params <- parameterize_steadystate(chem.cas = chem.cas,
+                                       chem.name = chem.name,
+                                       species = species,
+                                       clint.pvalue.threshold = clint.pvalue.threshold,
+                                       default.to.human = default.to.human,
+                                       human.clint.fup = human.clint.fup,
+                                       adjusted.Funbound.plasma = adjusted.Funbound.plasma,
+                                       restrictive.clearance = restrictive.clearance,
+                                       fup.lod.default = fup.lod.default,
+                                       suppress.messages = suppress.messages,
+                                       minimum.Funbound.plasma = minimum.Funbound.plasma,
+                                       Caco2.options = list(Caco2.Pab.default = Caco2.options$Caco2.Pab.default,
+                                                            Caco2.Fgut = FALSE,
+                                                            Caco2.Fabs = FALSE))
   }
 
   fabs.oral <- calc_fabs.oral(Params = Params)  
-  fhep.oral <- calc_hepatic.bioavailability(Params = Params)
+  fhep.oral <- Params$hepatic.bioavailability
   fgut.oral <- calc_fgut.oral(Params = Params)
   fbio.oral <- fabs.oral*fhep.oral*fgut.oral
   
-  return(fbio.oral)
+  return(list("fbio.oral" = fbio.oral,
+              "fabs.oral" = fabs.oral,
+              "fgut.oral" = fgut.oral,
+              "fhep.oral" = fhep.oral
+              ))
 
 }
 
@@ -107,17 +115,19 @@ calc_fabs.oral <- function(Params = NULL,
                            fup.lod.default=0.005,
                            suppress.messages=F,
                            minimum.Funbound.plasma=0.0001,
-                           Caco2.Pab.default = 1
+                           Caco2.options = list(Caco2.Pab.default = 2,
+                                                Caco2.Fgut = TRUE,
+                                                Caco2.Fabs = TRUE)
 ){
   # Required parameters
-  req.param <- c("Qtotal.liverc", "BW", "cl_us", "Rblood2plasma", "Caco2.Pab")
+  req.param <- c("Caco2.Pab")
   
   # Header initialization  
   if(is.null(Params) || all(!req.param %in% names(Params))){
     out <- get_chem_id(chem.cas=chem.cas,chem.name=chem.name)
     chem.cas <- out$chem.cas
     chem.name <- out$chem.name
-    Params <- parameterize_fbio(chem.cas = chem.cas,
+    Params <- parameterize_steadystate(chem.cas = chem.cas,
                                 chem.name = chem.name,
                                 species = species,
                                 clint.pvalue.threshold = clint.pvalue.threshold,
@@ -128,7 +138,9 @@ calc_fabs.oral <- function(Params = NULL,
                                 fup.lod.default = fup.lod.default,
                                 suppress.messages = suppress.messages,
                                 minimum.Funbound.plasma = minimum.Funbound.plasma,
-                                Caco2.Pab.default = Caco2.Pab.default)
+                                Caco2.options = list(Caco2.Pab.default = Caco2.options$Caco2.Pab.default,
+                                                     Caco2.Fgut = FALSE,
+                                                     Caco2.Fabs = FALSE))
   }
   
   
@@ -152,7 +164,9 @@ calc_fgut.oral <- function(Params = NULL,
                            fup.lod.default=0.005,
                            suppress.messages=F,
                            minimum.Funbound.plasma=0.0001,
-                           Caco2.Pab.default = 1
+                           Caco2.options = list(Caco2.Pab.default = 2,
+                                                Caco2.Fgut = TRUE,
+                                                Caco2.Fabs = TRUE)
 ){
   # Required parameters
   req.param <- c("BW", "cl_us", "Caco2.Pab")
@@ -162,7 +176,7 @@ calc_fgut.oral <- function(Params = NULL,
     out <- get_chem_id(chem.cas=chem.cas,chem.name=chem.name)
     chem.cas <- out$chem.cas
     chem.name <- out$chem.name
-    Params <- parameterize_fbio(chem.cas = chem.cas,
+    Params <- parameterize_steadystate(chem.cas = chem.cas,
                                 chem.name = chem.name,
                                 species = species,
                                 clint.pvalue.threshold = clint.pvalue.threshold,
@@ -173,7 +187,9 @@ calc_fgut.oral <- function(Params = NULL,
                                 fup.lod.default = fup.lod.default,
                                 suppress.messages = suppress.messages,
                                 minimum.Funbound.plasma = minimum.Funbound.plasma,
-                                Caco2.Pab.default = Caco2.Pab.default)
+                                Caco2.options = list(Caco2.Pab.default = Caco2.options$Caco2.Pab.default,
+                                                     Caco2.Fgut = FALSE,
+                                                     Caco2.Fabs = FALSE))
   }
   
   clu_hep <- Params$cl_us*Params$BW # L/h for 70 kg human
@@ -195,257 +211,6 @@ calc_fgut.oral <- function(Params = NULL,
   return(fgut.oral)
   
 }
-
-calc_hepatic.bioavailability <- function(Params = NULL,
-                           chem.cas = NULL,
-                           chem.name = NULL,
-                           species = "Human",
-                           clint.pvalue.threshold=0.05,
-                           default.to.human=F,
-                           human.clint.fup=F,
-                           adjusted.Funbound.plasma=T,
-                           restrictive.clearance=T,
-                           fup.lod.default=0.005,
-                           suppress.messages=F,
-                           minimum.Funbound.plasma=0.0001,
-                           Caco2.Pab.default = 1
-){
-  # Required parameters
-  req.param <- c("Qtotal.liverc", "BW", "cl_us", "Rblood2plasma")
-  
-  # Header initialization  
-  if(is.null(Params) || all(!req.param %in% names(Params))){
-    out <- get_chem_id(chem.cas=chem.cas,chem.name=chem.name)
-    chem.cas <- out$chem.cas
-    chem.name <- out$chem.name
-    Params <- parameterize_fbio(chem.cas = chem.cas,
-                                     chem.name = chem.name,
-                                     species = species,
-                                     clint.pvalue.threshold = clint.pvalue.threshold,
-                                     default.to.human = default.to.human,
-                                     human.clint.fup = human.clint.fup,
-                                     adjusted.Funbound.plasma = adjusted.Funbound.plasma,
-                                     restrictive.clearance = restrictive.clearance,
-                                     fup.lod.default = fup.lod.default,
-                                     suppress.messages = suppress.messages,
-                                     minimum.Funbound.plasma = minimum.Funbound.plasma,
-                                     Caco2.Pab.default = Caco2.Pab.default)
-  }
-  
-  Qliver <- Params$Qtotal.liverc / Params$BW^.25 #L/h/kg body weight
-  
-  if (restrictive.clearance) 
-  { 
-    Params[['hepatic.bioavailability']] <- 
-      Qliver / (Qliver + Params$Funbound.plasma * Params$cl_us / Params$Rblood2plasma) # Units cancel (i.e., unitless)
-  } else {
-    Params[['hepatic.bioavailability']] <- 
-      Qliver / (Qliver + Params$cl_us / Params$Rblood2plasma) # Units cancel (i.e., unitless)
-  }
-  
-  return(Params[['hepatic.bioavailability']])
-}
-  
-# Parameterize bioavailability functions. Should match parameterize_steady_state.
-parameterize_fbio <- function(chem.cas=NULL,
-                              chem.name=NULL,
-                              species="Human",
-                              clint.pvalue.threshold=0.05,
-                              default.to.human=F,
-                              human.clint.fup=F,
-                              adjusted.Funbound.plasma=T,
-                              restrictive.clearance=T,
-                              fup.lod.default=0.005,
-                              suppress.messages=F,
-                              minimum.Funbound.plasma=0.0001,
-                              Caco2.Pab.default = 1)
-{
-  Parameter <- Species <- variable <- Tissue <- NULL
-  physiology.data <- physiology.data
-  tissue.data <- tissue.data
-  # Look up the chemical name/CAS, depending on what was provide:
-  out <- get_chem_id(chem.cas=chem.cas,chem.name=chem.name)
-  chem.cas <- out$chem.cas
-  chem.name <- out$chem.name
-  
-  #Capitilie the first letter of spcies only:
-  species <- tolower(species)
-  substring(species,1,1) <- toupper(substring(species,1,1))
-  
-  if (!(species %in% colnames(physiology.data)))
-  {
-    if (toupper(species) %in% toupper(colnames(physiology.data)))
-    {
-      phys.species <- colnames(physiology.data)[toupper(colnames(physiology.data))==toupper(species)]
-    } else stop(paste("Physiological PK data for",species,"not found."))
-  } else phys.species <- species
-  this.phys.data <- physiology.data[,phys.species]
-  names(this.phys.data) <- physiology.data[,1]
-  
-  QGFRc <- this.phys.data[["GFR"]] #mL/min/kgBW
-  BW <- this.phys.data[["Average BW"]]
-  
-  
-  Qtotal.liverc <- subset(tissue.data,
-                          tolower(Species) == tolower(species) & 
-                            variable == 'Flow (mL/min/kg^(3/4))' & 
-                            Tissue == 'liver')[,'value']  #mL/min/kgBW^3/4
-  Vliverc <- subset(tissue.data,tolower(Species) == tolower(species) & variable == 'Vol (L/kg)' & Tissue == 'liver')[,'value'] # L/kg BW
-  
-  # Rate of disappearance of compound from a hepatocyte incubation
-  # (hepatic intrinsic clearance -- uL/min/million hepatocytes):
-  Clint.db <- try(get_invitroPK_param("Clint",species,chem.CAS=chem.cas),silent=T)
-  # Check that the trend in the CLint assay was significant:
-  Clint.pValue <- try(get_invitroPK_param("Clint.pValue",species,chem.CAS=chem.cas),silent=T)
-  if (class(Clint.db) == "try-error" & default.to.human || human.clint.fup) 
-  {
-    Clint.db <- try(get_invitroPK_param("Clint","Human",chem.CAS=chem.cas),silent=T)
-    Clint.pValue <- try(get_invitroPK_param("Clint.pValue","Human",chem.CAS=chem.cas),silent=T)
-    warning(paste(species,"coerced to Human for metabolic clerance data."))
-  }
-  if (class(Clint.db) == "try-error") 
-    stop("Missing metabolic clearance data for given species. Set default.to.human to true to substitute human value.")
-  # Check if clintis a point value or a distribution, if a distribution, use the median:
-  if (nchar(Clint.db) - nchar(gsub(",","",Clint.db))==3) 
-  {
-    Clint.dist <- Clint.db
-    Clint.point <- as.numeric(strsplit(Clint.db,",")[[1]][1])
-    Clint.pValue <- as.numeric(strsplit(Clint.db,",")[[1]][4])
-    if (!suppress.messages) warning("Clint is provided as a distribution.")
-  } else {
-    Clint.point <- Clint.db
-    Clint.dist <- NA
-  }
-  if (!is.na(Clint.pValue) & Clint.pValue > clint.pvalue.threshold) Clint.point  <- 0
-  
-  
-  # Caco-2 Pab:
-  Caco2.Pab.db <- try(get_invitroPK_param("Caco2.Pab", species = "Human", chem.CAS = chem.cas), silent = T)
-  if (class(Caco2.Pab.db) == "try-error"){  
-    Caco2.Pab.point <- Caco2.Pab.default
-    Caco2.Pab.dist <- NA
-    warning(paste0("Default value of ", Caco2.Pab.default, " used for Caco2 permeability."))
-  }
-    # Check if Caco2 a point value or a distribution, if a distribution, use the median:
-  if (nchar(Caco2.Pab.db) - nchar(gsub(",","",Caco2.Pab.db)) >= 1) 
-  {
-    Caco2.Pab.dist <- Caco2.Pab.db
-    Caco2.Pab.point <- as.numeric(strsplit(Caco2.Pab.db,",")[[1]][1])
-    if (!suppress.messages) warning("Clint is provided as a distribution.")
-  } else {
-    Caco2.Pab.point <- Caco2.Pab.db
-    Caco2.Pab.dist <- NA
-  }
-  
-
-    
-  
-  # unitless fraction of chemical unbound with plasma
-  # fup.db contains whatever was in the chem.phys table
-  fup.db <- try(get_invitroPK_param("Funbound.plasma",species,chem.CAS=chem.cas),silent=T)
-  if (class(fup.db) == "try-error" & default.to.human || human.clint.fup) 
-  {
-    fup.db<- try(get_invitroPK_param("Funbound.plasma","Human",chem.CAS=chem.cas),silent=T)
-    if (!suppress.messages) warning(paste(species,"coerced to Human for protein binding data."))
-  }
-  if (class(fup.db) == "try-error") stop("Missing protein binding data for given species. Set default.to.human to true to substitute human value.")
-  # Check if fup is a point value or a distribution, if a distribution, use the median:
-  if (nchar(fup.db) - nchar(gsub(",","",fup.db))==2) 
-  {
-    fup.point <- as.numeric(strsplit(fup.db,",")[[1]][1])
-    fup.dist <- fup.db
-    if (!suppress.messages) warning("Fraction unbound is provided as a distribution.")
-  } else {
-    fup.point <- fup.db
-    fup.dist <- NA
-  }
-  if (fup.point == 0)
-  {
-    fup.point <- fup.lod.default
-    if (!suppress.messages) warning("Fraction unbound = 0, changed to 0.005.")
-  }
-  
-  pKa_Donor <- suppressWarnings(get_physchem_param("pKa_Donor",chem.CAS=chem.cas)) # acid dissociation constants
-  pKa_Accept <- suppressWarnings(get_physchem_param("pKa_Accept",chem.CAS=chem.cas)) # basic association cosntants
-  Pow <- 10^get_physchem_param("logP",chem.CAS=chem.cas) # Octanol:water partition coeffiecient
-  ion <- calc_ionization(pH=7.4,pKa_Donor=pKa_Donor,pKa_Accept=pKa_Accept)
-  dow <- Pow * (ion$fraction_neutral + 0.001 * ion$fraction_charged + ion$fraction_zwitter)
-  
-  if (adjusted.Funbound.plasma)
-  {
-    if (human.clint.fup) 
-    {
-      Flipid <- subset(physiology.data,
-                       Parameter=='Plasma Effective Neutral Lipid Volume Fraction')[,
-                                                                                    which(colnames(physiology.data) == 'Human')]
-    } else {
-      Flipid <- subset(physiology.data,
-                       Parameter=='Plasma Effective Neutral Lipid Volume Fraction')[,
-                                                                                    which(tolower(colnames(physiology.data)) == tolower(species))]
-    }
-    fup.adjustment <- max(1 / ((dow) * Flipid + 1 / fup.point)/fup.point,
-                          minimum.Funbound.plasma) # Enforcing a sanity check on 
-    # plasma binding
-    fup.adjusted <- fup.point*fup.adjustment # unitless fraction
-    if (!suppress.messages) warning('Funbound.plasma adjusted for in vitro partitioning (Pearce, 2017).  Set adjusted.Funbound.plasma to FALSE to use original value.')
-  } else {
-    fup.adjusted <- fup.point
-    fup.adjustment <- NA
-  }
-  # Restrict values of fup:
-  if (fup.adjusted < minimum.Funbound.plasma) fup.adjusted <- minimum.Funbound.plasma
-  
-  Fgutabs <- try(get_invitroPK_param("Fgutabs",species,chem.CAS=chem.cas),silent=T)
-  if (class(Fgutabs) == "try-error") Fgutabs <- 1
-  
-  Params <- list()
-  Params[["Clint"]] <- Clint.point # uL/min/10^6
-  Params[["Clint.dist"]] <- Clint.dist
-  Params[["Caco2.Pab"]] <- Caco2.Pab.point # log10( 10 ^ -6 cm/s)
-  Params[["Caco2.Pab.dist"]] <- Caco2.Pab.dist
-  Params[["Funbound.plasma.adjustment"]] <- fup.adjustment
-  Params[["Funbound.plasma"]] <- fup.adjusted
-  Params[["Funbound.plasma.dist"]] <- fup.dist
-  Params[["Qtotal.liverc"]] <- Qtotal.liverc/1000*60     #        L/h/kgBW
-  Params[["Qgfrc"]] <- QGFRc/1000*60 #        L/h/kgBW     
-  Params[["Dow74"]] <- dow # unitless istribution coefficient at plasma pH 7.4
-  Params[["BW"]] <- BW # kg
-  Params[["MW"]] <- get_physchem_param("MW",chem.CAS=chem.cas) # molecular weight g/mol
-  # Correct for unbound fraction of chemical in the hepatocyte intrinsic clearance assay (Kilford et al., 2008)
-  Params[["Fhep.assay.correction"]] <- calc_fu_hep(Pow,
-                                                   pKa_Donor=pKa_Donor,
-                                                   pKa_Accept=pKa_Accept) # fraction 
-  Params[["million.cells.per.gliver"]] <- 110 # 10^6 cells/g-liver
-  Params[["Vliverc"]] <- Vliverc # L/kg BW
-  Params[["liver.density"]] <- 1.05 # g/mL
-  Params[['Fgutabs']] <- Fgutabs
-  
-  Rb2p <- available_rblood2plasma(chem.name=chem.name,
-                                  chem.cas=chem.cas,
-                                  species=species,
-                                  adjusted.Funbound.plasma=adjusted.Funbound.plasma,
-                                  suppress.messages=T)
-  Params[["Rblood2plasma"]] <- Rb2p
-  
-  # Calculate and return unscaled clearance
-  Params[["cl_us"]] <- calc_hepatic_clearance(parameters=Params,
-                                              hepatic.model='unscaled',
-                                              suppress.messages=T)#L/h/kg body weight
-  return(Params)
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
