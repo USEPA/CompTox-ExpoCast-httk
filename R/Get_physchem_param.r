d622ae9b8be65534d494cc637496f9f2eae1644c
greg_honda <honda.gregory@epa.gov>
2019-06-14 14:33:09 -0400

    Including updated Armitage

--- R/Get_physchem_param.r
+++ R/Get_physchem_param.r
@@ -1,8 +1,10 @@
 # This function retrives physico-chemical properties ("param") for the chemical specified by chem.name or chem.CAS from the vLiver tables.
-get_physchem_param <- function(param,chem.name=NULL,chem.CAS=NULL)
+get_physchem_param <- function(param, chem.name=NULL, chem.CAS=NULL)
 {
 
   chem.physical_and_invitro.data <- chem.physical_and_invitro.data
+  chem.CAS0 <- chem.CAS
+  chem.name0 <- chem.name
   if(is.null(chem.CAS) & is.null(chem.name)){
     stop("Must specifiy compound name or CAS.\n")
   }else if((!is.null(chem.CAS) & !any(chem.physical_and_invitro.data[,"CAS"] %in% chem.CAS)) & 
@@ -14,11 +16,21 @@ get_physchem_param <- function(param,chem.name=NULL,chem.CAS=NULL)
     }else{
       chem.CAS <- chem.physical_and_invitro.data[match(chem.name, chem.physical_and_invitro.data[,"Compound"]),"CAS"]
     }
+    
+    if(!is.null(chem.CAS0) & any(is.na(chem.name))){
+      stop(paste("CAS not matched in chem.physical_and_invitro.data for input CAS:", paste(chem.CAS0[is.na(chem.name)], collapse = ",")))
+    }
+    if(!is.null(chem.name0) & any(is.na(chem.CAS))){
+      stop(paste("Compound name not matched in chem.physical_and_invitro.data for input Compounds:", paste(chem.name0[is.na(chem.CAS)], collapse = ",")))
+    }
+    
+    
     if(!all(param %in% c("MW","logP","pKa_Donor","pKa_Accept",'logMA',"logP","logHenry","logWSol","MP"))){
       stop(paste("Parameter",param,"not among \"MW\", \"logP\", \"logMA\", \"logHenry\", \"logWSol\", \"MP\", \"pKa_Donor\", and \"pKa_Accept\".\n"))
     }
     this.index <- match(chem.CAS, chem.physical_and_invitro.data[,"CAS"])
-    if(!any(is.na(suppressWarnings(chem.physical_and_invitro.data[this.index,param]))) | 
+    if(!any(is.na(suppressWarnings(chem.physical_and_invitro.data[this.index,
+                                                                  param[!param %in% c("pKa_Accept","pKa_Donor", "logMA")]]))) | 
        any(param %in% c("pKa_Donor","pKa_Accept","logMA"))){
       values <- chem.physical_and_invitro.data[this.index,param]
       if(any(!param %in% c("pKa_Accept", "pKa_Donor"))){
@@ -61,11 +73,38 @@ get_physchem_param <- function(param,chem.name=NULL,chem.CAS=NULL)
           return(unlist(values.out))
         }
       }
-
-    }else stop(paste("Incomplete phys-chem data for ",
-                     paste(chem.name, collapse = ","),
+      
+    }else{
+      
+      if(length(this.index) == 1 & length(param) == 1){
+        stop(paste0("Incomplete phys-chem data for ",
+                     chem.name,
                      " -- missing ",
-                     paste(param, collapse = ","),"."))
+                     param,"."))
+      }else{
+        missing.param <- which(is.na(chem.physical_and_invitro.data[
+          this.index,param[!param %in% c("pKa_Accept", "pKa_Donor", "logMA")]]), arr.ind = T)
+        
+        if(length(this.index) >= 1 & length(param) > 1){
+           
+          stop(paste0("Missing phys-chem data for combinations of: \n",
+                      paste(lapply(unique(missing.param[,1]), 
+                 function(x) paste0(chem.CAS[x], ": ", 
+                                    paste(param[!param %in% c("pKa_Accept", "pKa_Donor", "logMA")][missing.param[missing.param[,1] %in% x,2]],
+                                          collapse = ", "))),
+                 collapse = "\n")))
+          
+         
+        }else if(length(this.index) > 1 & length(param == 1)){
+          
+          stop(paste0("Incomplete phys-chem data for ", param, " for: \n",
+                     paste(chem.CAS[missing.param], collapse = ",")
+                     ))
+
+        }
+      }
+      
+    }
   }
 }
 
