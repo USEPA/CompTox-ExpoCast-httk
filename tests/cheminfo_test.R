4f4e0008d8bf19710e6a641fe11a280252484d0e
Wambaugh <Wambaugh.John@epa.gov>
2019-06-17 18:02:38 -0400

    Added check for duplicated entries to the tests

--- tests/cheminfo_test.R
+++ tests/cheminfo_test.R
@@ -1,8 +1,16 @@
 #R CMD BATCH cheminfo_test.R cheminfo_test.Rout
 library(httk)
+# Check if the number of chemicals has changed:
 Css.list <- get_cheminfo()
 pbpk.list <- get_cheminfo(model='pbtk')
 rat.list <- get_cheminfo(species="rat")
 length(Css.list)
 length(pbpk.list)
 length(rat.list)
+                                                 
+# check for duplicate entries:
+length(unique(chem.physical_and_invitro.data$CAS)) == dim(chem.physical_and_invitro.data)[1]
+length(unique(chem.physical_and_invitro.data$Compound)) == dim(chem.physical_and_invitro.data)[1]
+length(unique(subset(chem.physical_and_invitro.data,!is.na(DTXSID))$DTXSID)) == 
+  dim(subset(chem.physical_and_invitro.data,!is.na(DTXSID)))[1]
+  
