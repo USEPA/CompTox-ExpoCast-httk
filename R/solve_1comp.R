d0c1a276f05faf1bb78414bebd988d1fd7b1889a
Wambaugh <Wambaugh.John@epa.gov>
2019-07-01 19:03:33 -0400

    Fixed problem with survey overwriting a function in graphics -- we only need to import the plot function from graphics (I think)

--- R/solve_1comp.R
+++ R/solve_1comp.R
@@ -80,7 +80,8 @@
 #' solve_1comp(chem.name='Bisphenol-A',days=1)
 #' params <- parameterize_1comp(chem.cas="80-05-7")
 #' solve_1comp(parameters=params)
-#' @import deSolve graphics
+#' @import deSolve 
+#' @importFrom graphics plot
 #' @export solve_1comp
 #' @useDynLib httk
 
