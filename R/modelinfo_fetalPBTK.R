2fa06d6447d6bb750dfae3dc575c1a55977552ca
msfeir <sfeir.mark@epa.gov>
2019-08-15 16:14:30 -0400

    Update prepregnant maternal body weight according to new parameterization scheme in Parameterize_fetal_pbtk.R. Begin edits to modelinfo_fetalPBTK.R.

--- R/modelinfo_fetalPBTK.R
+++ R/modelinfo_fetalPBTK.R
@@ -1,200 +1,204 @@
-#Note some variables, like hematocrit, employed with distributions elsewhere 
-#in httk, are not similarly treated or included in parameter lists here in the
-#pregnancy case for the sake of simplicity due to the the absence of data to 
-#better support anything other than simulation of the average case. 
+# Add this model to the list of models:
 
-#Compile list of useful parameter names for direct access, though these not
-#all directly passed to ODE solver. This list also contains the names of 
-#equivalent variables to those used in ode solving with modelfetalPBTK.c, as 
-#they are found named differently elsewhere in httk prior to being bridged
-#over in name for incorporation into the ode model solving process. 
-param.names.fetal.pbtk = c("pre_pregnant_BW", 
-                           "Clmetabolismc", 
-                           "Kgutabs",
-                           "Kkidney2pu",
-                           "Kliver2pu",
-                           "Krbc2pu", 
-                           "Kadipose2pu", 
-                           "Krest2pu", 
-                           "Klung2pu", 
-                           "Kgut2pu",
-                           "Kthyroid2pu", 
-                           "Kplacenta2pu", 
-                           "Kfplacenta2pu",
-                           "Kfkidney2pu", 
-                           "Kfrest2pu",
-                           "Kfthyroid2pu",
-                           "Kfliver2pu", 
-                           "Kflung2pu", 
-                           "Kfgut2pu", 
-                           "Kfbrain2pu", 
-                           "Vartc",
-                           "Vvenc",
-                           "Vgutc",
-                           "Vkidneyc",
-                           "Vliverc",
-                           "Vlungc",
-                           "Vthyroidc",
-                           "Clint",
-                           "Funbound.plasma",
-                           "Funbound.plasma.dist",
-                           "Funbound.plasma.adjustment",
-                           "Fgutabs",
-                           "Fhep.assay.correction",
-                           "liver.density",
-                           "million.cells.per.gliver",
-                           "MW",
-                           "Pow",
-                           "pKa_Donor",
-                           "pKa_Accept",
-                           "MA",
-                           "Rblood2pu")
+#Analytic expression for steady-state plasma concentration.
+#model.list[["fetal_pbtk"]]$analytic.css.func <- "calc_analytic_css_fetal_pbtk" <function not yet developed
 
-#Parameter names as used explicitly by the .c solver. 
-param.names.fetal.pbtk.solver = c("pre_pregnant_BW", 
-                                  "Clmetabolismc", 
-                                  "Kgutabs",
-                                  "Kkidney2pu",
-                                  "Kliver2pu",
-                                  "Krbc2pu", 
-                                  "Kadipose2pu", 
-                                  "Krest2pu", 
-                                  "Klung2pu", 
-                                  "Kgut2pu",
-                                  "Kthyroid2pu", 
-                                  "Kplacenta2pu", 
-                                  "Kfplacenta2pu",
-                                  "Kfkidney2pu", 
-                                  "Kfrest2pu",
-                                  "Kfthyroid2pu",
-                                  "Kfliver2pu", 
-                                  "Kflung2pu", 
-                                  "Kfgut2pu", 
-                                  "Kfbrain2pu", 
-                                  "Vartc",
-                                  "Vvenc",
-                                  "Vgutc",
-                                  "Vkidneyc",
-                                  "Vliverc",
-                                  "Vlungc",
-                                  "Vthyroidc",
-                                  "Fraction_unbound_plasma",
-                                  "Ratioblood2plasma")
+# The is the R function for generating model parameters:
+model.list[["fetal_pbtk"]]$parameterize.func <- "parameterize_fetal_pbtk"
 
+# These are all the parameters returned by the R model parameterization function.
+# Some of these parameters are not directly used to solve the model, but describe
+# how other parameters were calculated:
+model.list[["fetal_pbtk"]]$param.names <- c(
+  "BW",
+  "Clint",
+  "Clmetabolismc",
+  "Funbound.plasma",
+  "Funbound.plasma.dist",
+  "Funbound.plasma.adjustment",
+  "Fgutabs",
+  "Fhep.assay.correction",
+  "hematocrit",
+  "Kgut2pu",
+  "kgutabs",
+  "Kkidney2pu",
+  "Kliver2pu",
+  "Klung2pu",
+  "Krbc2pu",
+  "Krest2pu",
+  "liver.density",
+  "million.cells.per.gliver",
+  "MW",
+  "Pow",
+  "pKa_Donor",
+  "pKa_Accept",
+  "MA",
+  "Qcardiacc",
+  "Qgfrc",
+  "Qgutf",
+  "Qkidneyf",
+  "Qliverf",
+  "Rblood2plasma",
+  "Vartc",
+  "Vgutc",
+  "Vkidneyc",
+  "Vliverc",
+  "Vlungc",
+  "Vrestc",
+  "Vvenc")
 
-
-initParmsfetalpbtk <- function(newParms = NULL) {
-  parms <- c(
-    pre_pregnant_BW = 61.103,
-    Clmetabolismc = 0.0,
-    Kgutabs = 1,
-    Kkidney2pu = 0.0,
-    Kliver2pu = 0.0,
-    Krbc2pu = 0.0,
-    Kadipose2pu = 0.0,
-    Krest2pu = 0.0,
-    Klung2pu = 0.0,
-    Kgut2pu = 0.0,
-    Kthyroid2pu = 0.0,
-    Kplacenta2pu = 0.0,
-    Kfplacenta2pu = 0.0,
-    Kfkidney2pu = 0.0,
-    Kfrest2pu = 0.0,
-    Kfthyroid2pu = 0.0,
-    Kfliver2pu = 0.0,
-    Kflung2pu = 0.0,
-    Kfgut2pu = 0.0,
-    Kfbrain2pu = 0.0,
-    Vartc = 0.0,
-    Vvenc = 0.0,
-    Vgutc = 0.0,
-    Vkidneyc = 0.0,
-    Vliverc = 0.0,
-    Vlungc = 0.0,
-    Vart = 0.0,
-    Vven = 0.0,
-    Vgut = 0.0,
-    Vkidney = 0.0,
-    Vliver = 0.0,
-    Vlung = 0.0,
-    Vthyroidc = 0.0,
-    Vthyroid = 0.0,
-    Fraction_unbound_plasma = 0.0,
-    Ratioblood2plasma = 0.0,
-    Clmetabolism = 0.0
-  )
-  if (!is.null(newParms)) {
-    if (!all(names(newParms) %in% c(names(parms)))) {
-      stop(paste("illegal parameter name(s):",names(newParms)[!(names(newParms) %in% c(names(parms)))],sep=" "))
-    }
-  }
-  if (!is.null(newParms))
-    parms[names(newParms)] <- newParms
-  out <- .C("getParmsfetalpbtk",  as.double(parms),
-            out=double(length(parms)),
-            as.integer(length(parms)))$out
-  names(out) <- names(parms)
-  out
-}
-
-Outputsfetalpbtk <- c(
-    "Cgut",
-    "Cliver",
-    "Cven",
-    "Clung",
-    "Cart",
-    "Cadipose",
-    "Crest",
-    "Ckidney",
-    "Cserum",
-    "Aserum",
-    "Cthyroid",
-    "Cplacenta",
-    "Cfliver",
-    "Cfven",
-    "Cfart",
-    "Cfgut",
-    "Cflung",
-    "Cfrest",
-    "Cfthyroid",
-    "Cfkidney",
-    "Cfbrain",
-    "Afserum",
-    "Cfserum"
+# This subset of R parameters are needed to initially parametrize the compiled
+# code for the solver: (must match ORDER under "parameters" in C code)
+model.list[["pbtk"]]$Rtosolvermap <- list(
+  BW="BW",
+  Clmetabolismc="Clmetabolismc",
+  hematocrit="hematocrit",
+  kgutabs="kgutabs",
+  Kkidney2pu="Kkidney2pu",
+  Kliver2pu="Kliver2pu",
+  Krest2pu="Krest2pu",
+  Kgut2pu="Kgut2pu",
+  Klung2pu="Klung2pu",
+  Qcardiacc="Qcardiacc",
+  Qgfrc="Qgfrc",
+  Qgutf="Qgutf",
+  Qkidney="Qkidneyf",
+  Qliverf="Qliverf",
+  Vartc="Vartc",
+  Vgutc="Vgutc",
+  Vkidneyc="Vkidneyc",
+  Vliverc="Vliverc",
+  Vlungc="Vlungc",
+  Vrestc="Vrestc",
+  Vvenc="Vvenc",
+  Fraction_unbound_plasma="Funbound.plasma",
+  Rblood2plasma="Rblood2plasma"
 )
 
-initStatesfetalpbtk <- function(parms, newStates = NULL) {
-  Y <- c(
-    Agutlumen = 0.0,
-    Agut = 0.0,
-    Aliver = 0.0,
-    Aven = 0.0,
-    Alung = 0.0,
-    Aart = 0.0,
-    Aadipose = 0.0,
-    Arest = 0.0,
-    Akidney = 0.0,
-    Atubules = 0.0,
-    Ametabolized = 0.0,
-    AUC = 0.0,
-    fAUC = 0.0,
-    Athyroid = 0.0,
-    Aplacenta = 0.0,
-    Afgut = 0.0,
-    Aflung = 0.0,
-    Afliver = 0.0,
-    Afven = 0.0,
-    Afart = 0.0,
-    Afrest = 0.0,
-    Afthyroid = 0.0,
-    Afkidney = 0.0,
-    Afbrain = 0.0
-  )
-  if (!is.null(newStates)) {
-    if (!all(names(newStates) %in% c(names(Y)))) {
-      stop("illegal state variable name in newStates")
-    }
-    Y[names(newStates)] <- newStates
-  }
-  Y
-}
+# This function translates the R model parameters into the compiled model
+# parameters:
+model.list[["pbtk"]]$compiled.parameters.init <- "getParmspbtk"
+
+# This is the ORDERED full list of parameters used by the compiled code to 
+# calculate the derivative of the system of equations describing the model 
+model.list[["pbtk"]]$compiled.param.names <- c(
+  "BW",
+  "Clmetabolismc",
+  "hematocrit",
+  "kgutabs",
+  "Kkidney2pu",
+  "Kliver2pu",
+  "Krest2pu",
+  "Kgut2pu",
+  "Klung2pu",
+  "Qcardiacc",
+  "Qgfrc",
+  "Qgutf",
+  "Qkidneyf",
+  "Qliverf",
+  "Vartc",
+  "Vgutc",
+  "Vkidneyc",
+  "Vliverc",
+  "Vlungc",
+  "Vrestc",
+  "Vvenc",
+  "Fraction_unbound_plasma",
+  "Rblood2plasma",
+  "Clmetabolism",
+  "Qcardiac",
+  "Qgfr",
+  "Qgut",
+  "Qkidney",
+  "Qliver",
+  "Qrest",
+  "Vart",
+  "Vgut",
+  "Vkidney",
+  "Vliver",
+  "Vlung",
+  "Vrest",
+  "Vven"
+)
+
+# This function initializes the state vector for the compiled model:
+model.list[["pbtk"]]$compiled.init.func <- "initmodpbtk"
+
+# This is the function that calculates the derviative of the model as a function
+# of time, state, and parameters:
+model.list[["pbtk"]]$derivative.func <- "derivspbtk"
+
+# This is the ORDERED list of variables returned by the derivative function
+# (from Model variables: Outputs):
+model.list[["pbtk"]]$derivative.output.names <- c(
+  "Cgut",
+  "Cliver",
+  "Cven",
+  "Clung",
+  "Cart",
+  "Crest",
+  "Ckidney",
+  "Cplasma",
+  "Aplasma"
+)
+
+model.list[["pbtk"]]$default.monitor.vars <- c(
+  "Cgut",
+  "Cliver",
+  "Cven",
+  "Clung",
+  "Cart",
+  "Crest",
+  "Ckidney",
+  "Cplasma",
+  "Atubules",
+  "Ametabolized",
+  "AUC"
+)
+
+# Allowable units:
+model.list[["pbtk"]]$allowed.units <- c('um', 'mg/l')
+
+# These parameters specify the exposure scenario simulated by the model:
+model.list[["pbtk"]]$dosing.params <- c("daily.dose",
+                                        "initial.dose",
+                                        "doses.per.day",
+                                        "dosing.matrix")
+model.list[["pbtk"]]$routes <- c("oral","iv")
+# We need to know which compartment gets the dose 
+model.list[["pbtk"]]$dose.variable <- list(oral="Agutlumen",
+                                           iv="Aven")
+# Can take the values "add" to add dose C1 <- C1 + dose,
+#"replace" to change the value C1 <- dose
+#or "multiply" to change the value to C1 <- C1*dose
+model.list[["pbtk"]]$dose.type <- list(oral="add",
+                                       iv="add")
+
+# This ORDERED LIST of variables are always calculated in amounts (must match
+# Model variables: States in C code): 
+model.list[["pbtk"]]$state.vars <- c(
+  "Agutlumen",
+  "Agut",
+  "Aliver",
+  "Aven",
+  "Alung",
+  "Aart",
+  "Arest",
+  "Akidney", 
+  "Atubules",
+  "Ametabolized",
+  "AUC"
+) 
+
+#Parameters needed to make a prediction (this is used by get_cheminfo):
+model.list[["pbtk"]]$required.params <- c(
+  "Clint",
+  "Funbound.plasma",
+  "Pow",
+  "pKa_Donor",
+  "pKa_Accept",
+  "MW"
+)
+
+# Do we ignore the Fups where the value was below the limit of detection?
+model.list[["pbtk"]]$exclude.fup.zero <- T
