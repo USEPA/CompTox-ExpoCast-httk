b0f725563c7721931bc2709212c4500ce58150a8
msfeir <sfeir.mark@epa.gov>
2019-08-15 17:00:32 -0400

    Comment out line of parameter initializations not included in current fetal model (e.g. Vbrain) in Parameterize_fetal_pbtk.R.

--- R/Parameterize_fetal_pbtk.R
+++ R/Parameterize_fetal_pbtk.R
@@ -101,7 +101,7 @@ parameterize_fetal_pbtk<- function(chem.cas=NULL,
  parms$Vartc <- 0.624/parms$pre_pregnant_BW         
  parms$Vvenc <- 2.32/parms$pre_pregnant_BW
  parms$Vfgutc <- 0.0178
-parms$Vrestc <- parms$BW <- parms$Qcardiacc <- parms$Qgutf <- parms$Qbrainf <- parms$Qlungf <- parms$Qliverf <- parms$Qkidneyf <- parms$Vbrainc <- parms$Kbrain2pu <- NULL  
+#parms$Vrestc <- parms$BW <- parms$Qcardiacc <- parms$Qgutf <- parms$Qbrainf <- parms$Qlungf <- parms$Qliverf <- parms$Qkidneyf <- parms$Vbrainc <- parms$Kbrain2pu <- NULL  
 #parms$fBW <- 0.00003107 * exp(0.8137/0.06458 * (1 - exp(-0.06458 * day / 7)))  
 #parms$Vplacenta <- 0.317 * parms$fBW^0.582  
  return(parms)                             
