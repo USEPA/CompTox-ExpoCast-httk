906f8e13e0613ce192124f40a762ef3e08241ee1
jwambaug <wambaugh.john@epa.gov>
2019-12-13 12:57:48 -0500

    Merging

--- R/modelinfo_1comp.R
+++ R/modelinfo_1comp.R
@@ -6,78 +6,6 @@
 # Analytic expression for steady-state plasma concentration.
 model.list[["1compartment"]]$analytic.css.func <- "calc_analytic_css_1comp"
 
-<<<<<<< HEAD
-#Define the parameter names for each model in one place so that all functions can use them:
-param.names.1comp <- c("BW",
-                     "Clint",
-                     "Clint.dist",
-                     "Fabsgut",
-                     "Fhep.assay.correction",
-                     "Funbound.plasma",
-                     "Funbound.plasma.dist",
-                     "Funbound.plasma.adjustment",
-                     "hepatic.bioavailability",
-                     "hematocrit",
-                     "kelim",
-                     "kgutabs",
-                     "liver.density",
-                     "million.cells.per.gliver",
-                     "MA",
-                     "MW",
-                     "Rblood2plasma",
-                     "Pow",
-                     "pKa_Donor",
-                     "pKa_Accept",
-                     "Vdist")
-
-param.names.1comp.solver <- c("vdist",
-                     "ke",
-                     "kgutabs")
-
-initparms1comp <- function(newParms = NULL){
-  parms <- c(
-    vdist = 0,
-    ke = 0,
-    kgutabs = 1
-  )
-  if (!is.null(newParms)) {
-    if (!all(names(newParms) %in% c(names(parms)))) {
-      stop("illegal parameter name")
-    }
-  }
-  if (!is.null(newParms)) parms[names(newParms)] <- newParms
-  out <- .C("getParms1comp",
-   as.double(parms),
-  out=double(length(parms)),
-  as.integer(length(parms)))$out
-  names(out) <- names(parms)
-  out
-}
-
-Outputs1comp <- c(
-    "Ccompartment"
-)
-
-
-initState1comp <- function(parms, newState = NULL) {
-  Y <- c(
-    Agutlumen = 0.0,
-    Acompartment = 0.0,
-    Ametabolized = 0.0,
-    AUC = 0.0
-  )
-  Y <- with(as.list(parms), {  Y
-  })
-
-  if (!is.null(newState)) {
-    if (!all(names(newState) %in% c(names(Y)))) {
-      stop("illegal state variable name in newState")
-    }
-    Y[names(newState)] <- newState
-  }
-  Y
-}
-=======
 # Function used for generating model parameters:
 model.list[["1compartment"]]$parameterize.func <- "parameterize_1comp"
 
@@ -225,4 +153,3 @@ model.list[["1compartment"]]$firstpass <- TRUE
 
 # Do we ignore the Fups where the value was below the limit of detection?
 model.list[["1compartment"]]$exclude.fup.zero <- T
->>>>>>> cd6935617acdc1f8696861a41ecfb6190cbebda1
--- R/modelinfo_3comp.R
+++ R/modelinfo_3comp.R
@@ -6,80 +6,6 @@
 #Analytic expression for steady-state plasma concentration.
 model.list[["3compartment"]]$analytic.css.func <- "calc_analytic_css_3comp"
 
-<<<<<<< HEAD
-#Define the parameter names for each model in one place so that all functions can use them:
-param.names.3comp <- c("BW",
-                     "Clint",
-                     "Clint.dist",
-                     "Clmetabolismc",
-                     "Funbound.plasma",
-                     "Funbound.plasma.dist",
-                     "Funbound.plasma.adjustment",
-                     "Fabsgut",
-                     "Fabs",
-                     "Fgut",
-                     "Fhep.assay.correction",
-                     "hematocrit",
-                     "Kgut2pu",
-                     "Krbc2pu",
-                     "kgutabs",
-                     "Kliver2pu",
-                     "Krest2pu",
-                     "liver.density",
-                     "million.cells.per.gliver",
-                     "MW",
-                     "Pow",
-                     "pKa_Donor",
-                     "pKa_Accept",
-                     "MA",
-                     "Qcardiacc",
-                     "Qgfrc",
-                     "Qgutf",
-                     "Qliverf",
-                     "Rblood2plasma",
-                     "Vgutc",
-                     "Vliverc",
-                     "Vrestc")
-
-param.names.3comp.solver <- c("BW",
-                     "Clmetabolismc",
-                     'Fraction_unbound_plasma',
-                     "Kgut2plasma",
-                     "kgutabs",
-                     "Kliver2plasma",
-                     "Krest2plasma",
-                     "Qcardiacc",
-                     "Qgfrc",
-                     "Qgutf",
-                     "Qliverf",
-                     "Ratioblood2plasma",
-                     "Vgut",
-                     "Vliver",
-                     "Vrest")
-
-initparms3comp <- function(newParms = NULL){
-  parms <- c(
-    BW = 70,
-    Clmetabolismc = 0.203,
-    kgutabs = 1,
-    Qcardiacc = 0,
-    Qgfrc = 0.108,
-    Qgutf = 0.205,
-    Qliverf = 0.0536,
-    Vgut = 0,
-    Vliver = 0,
-    Vrest = 0,
-    Fraction_unbound_plasma = 0.0682,
-    Clmetabolism = 0.0,
-    Qcardiac = 0,
-    Qgfr = 0.0,
-    Qgut = 0.0,
-    Qliver = 0.0,
-    Kliver2plasma = 0,
-    Krest2plasma = 0,
-    Kgut2plasma = 0,
-    Ratioblood2plasma = 0
-=======
 # Function used for generating model parameters:
 model.list[["3compartment"]]$parameterize.func <- "parameterize_3comp"
 
@@ -178,7 +104,6 @@ model.list[["3compartment"]]$compiled.param.names <- c(
   "Kliver2plasma",
   "Krest2plasma",
   "Ratioblood2plasma"
->>>>>>> cd6935617acdc1f8696861a41ecfb6190cbebda1
   )
 
 # This function initializes the state vector for the compiled model:
