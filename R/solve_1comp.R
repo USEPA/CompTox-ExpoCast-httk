2ff0448fd892aaa23818152ed0c4e38167138c43
jwambaug <wambaugh.john@epa.gov>
2019-12-13 13:11:53 -0500

    Merging

--- R/solve_1comp.R
+++ R/solve_1comp.R
@@ -94,113 +94,6 @@
 #'
 #' @export solve_1comp
 #' @useDynLib httk
-<<<<<<< HEAD
-
-solve_1comp <- function(chem.name=NULL,
-                        chem.cas=NULL,
-                        times=NULL,
-                        parameters=NULL,
-                        daily.dose=1,
-                        dose = NULL,
-                        doses.per.day=NULL,
-                        days=10,
-                        tsteps=4,
-                        suppress.messages=F,
-                        species='Human',
-                        output.units='uM',
-                        plots=F,
-                        initial.values=NULL,
-                        iv.dose = F,
-                        method="lsoda",rtol=1e-8,atol=1e-12,
-                        default.to.human=F,
-                        dosing.matrix = NULL,
-                        recalc.elimination=F,
-                        adjusted.Funbound.plasma=T,
-                        regression=T,
-                        restrictive.clearance=T,
-                        well.stirred.correction=T,
-                        minimum.Funbound.plasma=0.0001,
-                        Caco2.options = list(Caco2.Pab.default = 1.6,
-                                             Caco2.Fgut = TRUE,
-                                             Caco2.Fabs = TRUE,
-                                             overwrite.invivo = FALSE,
-                                             keepit100 = FALSE),
-                        ...)
-{     
-   Agutlumen <- Acompartment <- Ccompartment <- NULL 
-  if (is.null(chem.cas) & is.null(chem.name) & is.null(parameters)) 
-    stop('Parameters, chem.name, or chem.cas must be specified.')
-  if (is.null(parameters))
-  {
-   parameters <- parameterize_1comp(chem.name=chem.name,
-                   chem.cas=chem.cas,
-                   species=species,
-                   default.to.human=default.to.human,
-                   adjusted.Funbound.plasma=adjusted.Funbound.plasma,
-                   regression=regression,
-                   restrictive.clearance=restrictive.clearance,
-                   well.stirred.correction=well.stirred.correction,
-                   suppress.messages=suppress.messages,
-                   minimum.Funbound.plasma=minimum.Funbound.plasma,
-                   Caco2.options = Caco2.options) 
-  } else {
-     if (!all(param.names.1comp %in% names(parameters)))
-       stop(paste("Missing parameters:",
-       paste(param.names.1comp[which(!param.names.1comp %in% names(parameters))],
-       collapse=', '),".  Use parameters from parameterize_1comp."))
-  }
-  Rb2p <- parameters[['Rblood2plasma']]
-  BW <- parameters[['BW']]
-   
-  parameters$Fbio.oral <- parameters$Fabsgut * parameters$hepatic.bioavailability
-
-  if(is.null(times)) times <- round(seq(0, days, 1/(24*tsteps)),8)
-  start <- times[1]
-  end <- times[length(times)] 
-    if(iv.dose) parameters$Fbio.oral <- 1
-    if(is.null(dosing.matrix)){ 
-      if(is.null(dose)){
-        if(!is.null(doses.per.day)){
-          dose <- daily.dose / doses.per.day * parameters$Fbio.oral
-        }else dose <- daily.dose * parameters$Fbio.oral
-      }else dose <- dose * parameters$Fbio.oral
-    }else{
-      if(!is.null(dim(dosing.matrix))){
-        rc <- which(dim(dosing.matrix) == 2)
-        if(rc == 1){
-          if(is.null(rownames(dosing.matrix)) | any(!(rownames(dosing.matrix) %in% c('time','dose')))) stop('dosing.matrix must have column or row names of \"time\" and \"dose\" or be a vector of times.')
-          dosing.times <- dosing.matrix['time',]
-          dose.vector <- dosing.matrix['dose',] * parameters$Fbio.oral
-        }else{
-          if(is.null(colnames(dosing.matrix)) | any(!(colnames(dosing.matrix) %in% c('time','dose')))) stop('dosing.matrix must have column or row names of \"time\" and \"dose\" or be a vector of times.')
-          dosing.times <- dosing.matrix[,'time']
-          dose.vector <- dosing.matrix[,'dose'] * parameters$Fbio.oral 
-        }
-      }else{
-        if(is.null(dose)) stop("Argument dose must be entered to overwrite daily.dose when a time vector is entered into dosing.matrix.")
-        dosing.times <- dosing.matrix
-        dose.vector <- rep(dose * parameters$Fbio.oral,length(dosing.matrix))
-      } 
-      if(start == dosing.times[1]){
-        dose <- dose.vector[[1]]
-        dosing.times <- dosing.times[2:length(dosing.times)]
-        dose.vector <- dose.vector[2:length(dose.vector)]
-      }else dose <- 0  
-    } 
-   
-  if(tolower(output.units)=='um' |  tolower(output.units) == 'mg/l') use.amounts <- F
-  if(tolower(output.units)=='umol' |  tolower(output.units) == 'mg') use.amounts <- T 
-   
-  if(tolower(output.units)=='um' | tolower(output.units) == 'umol')
-  {
-    dose <- as.numeric(dose / 1000 / parameters[["MW"]] * 1000000)
-    if(!is.null(dosing.matrix)) dose.vector <- as.numeric(dose.vector / 1000 / parameters[["MW"]] * 1000000)
-  } else if(!(tolower(output.units) == 'mg/l' | tolower(output.units) == 'mg'))
-  {
-    stop('Output.units can only be uM, umol, mg, or mg/L.')
-
-  }
-=======
 solve_1comp <- function(chem.name = NULL,
                     chem.cas = NULL,
                     times=NULL,
@@ -226,6 +119,11 @@ solve_1comp <- function(chem.name = NULL,
                     restrictive.clearance = T,
                     minimum.Funbound.plasma=0.0001,
                     monitor.vars=NULL,
+                    Caco2.options = list(Caco2.Pab.default = 1.6,
+                      Caco2.Fgut = TRUE,
+                      Caco2.Fabs = TRUE,
+                      overwrite.invivo = FALSE,
+                      keepit100 = FALSE),
                     ...)
 {
   out <- solve_model(
@@ -257,8 +155,8 @@ solve_1comp <- function(chem.name = NULL,
     regression=regression,
     restrictive.clearance = restrictive.clearance,
     minimum.Funbound.plasma=minimum.Funbound.plasma,
+    Caco2.options=Caco2.options,
     ...)
->>>>>>> cd6935617acdc1f8696861a41ecfb6190cbebda1
   
   return(out) 
 }
