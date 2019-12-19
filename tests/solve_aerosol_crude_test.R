0dcd6a9da9017b82081f1f893bd722136f01fcc1
msfeir <sfeir.mark@epa.gov>
2019-12-19 13:30:27 -0500

    Remove test files for aerosol functionality from this branch, run R CMD BATCH to obtain benchmark output for solve_gas_pbtk.R.

--- tests/solve_aerosol_crude_test.R
+++
@@ -1,9 +0,0 @@
-#Branch specific 'solve' function tests 
-library(httk)
-
-head(solve_aerosol_pbtk(chem.name="pyrene"))
-head(solve_aerosol_pbtk(chem.cas="129-00-0"))
-head(solve_aerosol_pbtk(parameters=parameterize_aerosol_pbtk(chem.cas="129-00-0")))
-
-
-
