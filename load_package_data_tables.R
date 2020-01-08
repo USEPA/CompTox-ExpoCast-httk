319189fa10b933db836830dc5391ba6aa2e4afd3
Wambaugh <Wambaugh.John@epa.gov>
2020-01-08 15:36:22 -0500

    Converted Wetmore 2015 tables to txt files

--- load_package_data_tables.R
+++ load_package_data_tables.R
@@ -87,7 +87,13 @@ chem.prop[chem.prop$Compound=="Bensulide",]
 sum(chem.prop$Compound=="dibutyl benzene-1,2-dicarboxylate")
 
 #
-WetmorePhaseII.fup.table <- read.table("Wetmore2015.fup.table.txt",stringsAsFactors=F)
+WetmorePhaseII.fup.table <- read.table("Wetmore2015.fup.table.txt",
+  stringsAsFactors=F,
+  fill=T,
+  header=T,
+  sep="\t",
+  quote = "",
+  comment.char = "")
 WetmorePhaseII.fup.table$X10.mM[WetmorePhaseII.fup.table$X10.mM>100] <- 100
 WetmorePhaseII.fup.table$X10.mM <- as.numeric(WetmorePhaseII.fup.table$X10.mM)/100
 #There were a couple of salts in this data set, let's replicate them as acids:
@@ -103,8 +109,8 @@ salt <- subset(WetmorePhaseII.fup.table,CAS=="2795-39-3")
 salt$CAS <- "3825-26-1"
 salt$Name <- "Perfluorooctanoic acid"
 WetmorePhaseII.fup.table <- rbind(WetmorePhaseII.fup.table,salt)
-salt <- subset(WetmorePhaseII.fup.table,CAS=="355-46-4")
-salt$CAS <- "3871-99-6"
+salt <- subset(WetmorePhaseII.fup.table,CAS=="3871-99-6")
+salt$CAS <- "355-46-4"
 salt$Name <- "Perfluorohexanesulfonic acid"
 WetmorePhaseII.fup.table <- rbind(WetmorePhaseII.fup.table,salt)
 chem.prop <- add_chemtable(WetmorePhaseII.fup.table,
@@ -116,7 +122,13 @@ chem.prop <- add_chemtable(WetmorePhaseII.fup.table,
                  Compound="Name",
                  Funbound.plasma="X10.mM"),overwrite=T)
                  
-WetmorePhaseII.clint.table <- read.table("Wetmore2015.clint.table.txt",,stringsAsFactors=F)
+WetmorePhaseII.clint.table <- read.table("Wetmore2015.clint.table.txt",
+  stringsAsFactors=F,
+  fill=T,
+  header=T,
+  sep="\t",
+  quote = "",
+  comment.char = "")
 WetmorePhaseII.clint.table <- subset(WetmorePhaseII.clint.table,Conc.=="1 uM")
 WetmorePhaseII.clint.table[,"P.value"] <-as.numeric(gsub("< ","",WetmorePhaseII.clint.table[,"P.value"]))
 WetmorePhaseII.clint.table[,"Adj.Cl"] <-as.numeric(WetmorePhaseII.clint.table[,"Adj.Cl"])
@@ -125,16 +137,16 @@ salt <- subset(WetmorePhaseII.clint.table,CAS=="2795-39-3")
 salt$CAS <- "1763-23-1"
 salt$Name <- "Perfluorooctanesulfonic acid"
 WetmorePhaseII.clint.table <- rbind(WetmorePhaseII.clint.table,salt)
-salt <- subset(WetmorePhaseII.fup.table,CAS=="29420-49-3")
+salt <- subset(WetmorePhaseII.clint.table,CAS=="29420-49-3")
 salt$CAS <- "375-73-5"
 salt$Name <- "Perfluorobutanesulfonic acid"
 WetmorePhaseII.clint.table <- rbind(WetmorePhaseII.clint.table,salt)
-salt <- subset(WetmorePhaseII.fup.table,CAS=="2795-39-3")
+salt <- subset(WetmorePhaseII.clint.table,CAS=="2795-39-3")
 salt$CAS <- "3825-26-1"
 salt$Name <- "Perfluorooctanoic acid"
 WetmorePhaseII.clint.table <- rbind(WetmorePhaseII.clint.table,salt)
-salt <- subset(WetmorePhaseII.fup.table,CAS=="355-46-4")
-salt$CAS <- "3871-99-6"
+salt <- subset(WetmorePhaseII.clint.table,CAS=="3871-99-6")
+salt$CAS <- "355-46-4"
 salt$Name <- "Perfluorohexanesulfonic acid"
 WetmorePhaseII.clint.table <- rbind(WetmorePhaseII.clint.table,salt)
 chem.prop <- add_chemtable(WetmorePhaseII.clint.table,                                        
@@ -773,7 +785,7 @@ write.table(chem.physical_and_invitro.data[,c("Compound","CAS")],
 cat("Chemical ID's written to HTTK-ChemIDs.txt, use that file to Batch Search based on CAS.\n")
 cat("Download CAS, MW, desalted (QSAR-ready) SMILES, forumula, DTXSIDs, and OPERA properties.\n")
 cat("Save Dashboard output to HTTK-DSSTox-output.xls.\n")
-cat("Enter \"c\" to continue when ready.\n"}
+cat("Enter \"c\" to continue when ready.\n")
 browser()
 
 #
@@ -817,7 +829,7 @@ write.table(subset(chem.physical_and_invitro.data,
 cat("Chemical with NA DTXSID's written to HTTK-NoCASMatch-ChemIDs.txt, use that file to search baed on CAS. \n")
 cat("Download CAS, MW, desalted (QSAR-ready) SMILES, forumula, DTXSIDs, and OPERA properties.\n")
 cat("Save Dashboard output to HTTK-NoNameMatch-DSSTox-output.xls.\n")
-cat("Enter \"c\" to continue when ready.\n"}
+cat("Enter \"c\" to continue when ready.\n")
 browser()
 dsstox <- read.xlsx("HTTK-NoCASMatch-DSSTox-output.xls",stringsAsFactors=F,1)
 # Get rid of the ones that weren't found:
