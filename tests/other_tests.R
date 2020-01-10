b6004860edc0057ef403b7a461953a8b8b40bc1d
jwambaug <wambaugh.john@epa.gov>
2020-01-10 12:22:57 -0500

    Checking into calc_stats

--- tests/other_tests.R
+++ tests/other_tests.R
@@ -3,7 +3,12 @@ library(httk)
 options(warn=-1)
 
 calc_css(chem.name='nicotine')
+
 calc_css(chem.name="perfluorooctanoic acid")
+
 calc_stats(chem.name='nicotine',days=10)
+
+calc_stats(chem.cas='94-75-7',days=10)
+
 calc_analytic_css(chem.name='Bisphenol-A',tissue='liver',species='rabbit',
                   default.to.human=TRUE,daily.dose=2)
