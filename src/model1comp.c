268f89385121559e573a7561eed207290c33f814
jwambaug <wambaugh.john@epa.gov>
2020-02-11 15:47:49 -0500

    It's okay if these models don't handle forcing functions

--- src/model1comp.c
+++ src/model1comp.c
@@ -61,25 +61,6 @@ double ytau[1] = {0.0};
 
 static double yini[4] = {0.0, 0.0, 0.0, 0.0}; /*Array of initial state variables*/
 
-void lagvalue(double T, int *nr, int N, double *ytau) {
-  static void(*fun)(double, int*, int, double*) = NULL;
-  if (fun == NULL)
-    fun = (void(*)(double, int*, int, double*))R_GetCCallable("deSolve", "lagvalue");
-  return fun(T, nr, N, ytau);
-}
-
-double CalcDelay(int hvar, double dTime, double delay) {
-  double T = dTime-delay;
-  if (dTime > delay){
-    nr[0] = hvar;
-    lagvalue( T, nr, Nout, ytau );
-}
-  else{
-    ytau[0] = yini[hvar];
-}
-  return(ytau[0]);
-}
-
 /*----- Initializers */
 void initmod1comp (void (* odeparms)(int *, double *))
 {
