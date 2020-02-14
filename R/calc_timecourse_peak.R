3de5276a14845ca80fbe5e13c15f78212c24a66b
Wambaugh <Wambaugh.John@epa.gov>
2020-02-13 19:09:21 -0500

    Trying to reduce size of vignettes

--- R/calc_timecourse_peak.R
+++ R/calc_timecourse_peak.R
@@ -1,5 +1,5 @@
 # This function calculates the peak concentration given a PK timecourse (concentration vs. time):
 calc_timecourse_peak <- function(timecourse,conc.col=2,time.col=1)
 {
-  return(max(timecourse[,conc.col]))
+  return(signif(max(timecourse[,conc.col]),4))
 }
