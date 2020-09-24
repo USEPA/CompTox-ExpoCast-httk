9c625bde7d494afa4d322719fd04f1dcbcafc8f8
Wambaugh <Wambaugh.John@epa.gov>
2020-09-24 13:55:16 -0400

    Revert "Trying to pass CRAN checks"
    This reverts commit 74618bb53311c089a5d53ab4ccbd455998d66114.

--- httk/R/httk-package.R
+++ httk/R/httk-package.R
@@ -44,44 +44,44 @@ NULL
 #' @author John Wambaugh, Robert Pearce, Caroline Ring, Gregory Honda, Nisha
 #' Sipes, Jimena Davis, Barbara Wetmore, Woodrow Setzer, Mark Sfeir
 #' @seealso
-#' \href{httpss://www.epa.gov/chemical-research/computational-toxicology-communities-practice-high-throughput-toxicokinetic-httk-r}{PowerPoint
+#' \href{https://www.epa.gov/chemical-research/computational-toxicology-communities-practice-high-throughput-toxicokinetic-httk-r}{PowerPoint
 #' Presentation: High-Throughput Toxicokinetics (HTTK) R package}
 #' 
-#' \href{httpss://doi.org/10.18637/jss.v079.i04}{Pearce et al. (2017): httk: R
+#' \href{https://doi.org/10.18637/jss.v079.i04}{Pearce et al. (2017): httk: R
 #' Package for High-Throughput Toxicokinetics}
 #' 
-#' \href{httpss://doi.org/10.1093/toxsci/kfv171}{Wetmore et al. (2015):
+#' \href{https://doi.org/10.1093/toxsci/kfv171}{Wetmore et al. (2015):
 #' Incorporating High-Throughput Exposure Predictions With Dosimetry-Adjusted
 #' In Vitro Bioactivity to Inform Chemical Toxicity Testing}
 #' 
-#' \href{httpss://doi.org/10.1093/toxsci/kfv118}{Wambaugh et al. (2015):
+#' \href{https://doi.org/10.1093/toxsci/kfv118}{Wambaugh et al. (2015):
 #' Toxicokinetic Triage for Environmental Chemicals}
 #' 
-#' \href{httpss://doi.org/10.1007/s10928-017-9548-7}{Pearce et al. (2017):
+#' \href{https://doi.org/10.1007/s10928-017-9548-7}{Pearce et al. (2017):
 #' Evaluation and calibration of high-throughput predictions of chemical
 #' distribution to tissues}
 #' 
-#' \href{httpss://doi.org/10.1016/j.envint.2017.06.004}{Ring et al. (2017):
+#' \href{https://doi.org/10.1016/j.envint.2017.06.004}{Ring et al. (2017):
 #' Identifying populations sensitive to environmental chemicals by simulating
 #' toxicokinetic variability}
 #' 
-#' \href{httpss://doi.org/10.1021/acs.est.7b00650}{Sipes et al. (2017): An
+#' \href{https://doi.org/10.1021/acs.est.7b00650}{Sipes et al. (2017): An
 #' Intuitive Approach for Predicting Potential Human Health Risk with the Tox21
 #' 10k Library}
 #' 
-#' \href{httpss://doi.org/10.1093/toxsci/kfy020}{Wambaugh et al. (2018):
+#' \href{https://doi.org/10.1093/toxsci/kfy020}{Wambaugh et al. (2018):
 #' Evaluating In Vitro-In Vivo Extrapolation of Toxicokinetics}
 #'
-#' \href{httpss://doi.org/10.1371/journal.pone.0217564}{Honda et al. (2019):
+#' \href{https://doi.org/10.1371/journal.pone.0217564}{Honda et al. (2019):
 #' Using the concordance of in vitro and in vivo data to evaluate extrapolation assumptionss}
 #' 
-#' \href{httpss://doi.org/10.1093/toxsci/kfz205}{Wambaugh et al. (2019):
+#' \href{https://doi.org/10.1093/toxsci/kfz205}{Wambaugh et al. (2019):
 #' Assessing Toxicokinetic Uncertainty and Variability in Risk Prioritization}
 #' 
-#' \href{httpss://doi.org/10.1038/s41370-020-0238-y}{Linakis et al. (2020):
+#' \href{https://doi.org/10.1038/s41370-020-0238-y}{Linakis et al. (2020):
 #' Development and evaluation of a high throughput inhalation model for organic chemicals}
 #' 
-#' \href{httpss://www.epa.gov/chemical-research/rapid-chemical-exposure-and-dose-research}{EPA's
+#' \href{https://www.epa.gov/chemical-research/rapid-chemical-exposure-and-dose-research}{EPA's
 #' ExpoCast (Exposure Forecasting) Project}
 #' @keywords package
 NULL
