3826733d016f79805807e58e7bd00a281bbfbf65
Sarah Davidson <davidson.sarah.e@epa.gov>
2021-04-20 10:21:07 -0400

    HTTK-7 subtask: add vector of chemical classes to modelinfo lists that should be filtered out, added class PFAS

--- httk/R/modelinfo_Schmitt.R
+++ httk/R/modelinfo_Schmitt.R
@@ -94,3 +94,6 @@ model.list[["schmitt"]]$exclude.fup.zero <- T
 
 # Filter out volatile compounds with Henry's Law Constant Threshold
 model.list[["schmitt"]]$log.henry.threshold <- c(-4.5)
+
+# Filter out compounds belonging to select chemical classes
+model.list[["schmitt"]]$chem.class.filt <- c("PFAS")
