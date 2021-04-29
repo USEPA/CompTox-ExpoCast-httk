e4e042ebb29bd07dc46a4bce49c43288545895db
Wambaugh <Wambaugh.John@epa.gov>
2021-04-28 21:17:23 -0400

    Needed to specify rest in alltissues

--- httk/R/modelinfo_3comp.R
+++ httk/R/modelinfo_3comp.R
@@ -30,7 +30,7 @@ model.list[["3compartment"]]$alltissues=c(
   "skin",            
   "spleen",          
   "red blood cells",
-  "thyroid")
+  "rest")
 
 # Which tissues from tissue.data are not lumped together when forming
 # the model: 3 compartment model has only liver and gut compartments; 
