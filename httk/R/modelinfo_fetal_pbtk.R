7aa47cc5135934bcd6083b642669f2d99be3a65f
Wambaugh <Wambaugh.John@epa.gov>
2021-10-20 07:54:49 -0400

    Ack! R2solvermap wasn't right

--- httk/R/modelinfo_fetal_pbtk.R
+++ httk/R/modelinfo_fetal_pbtk.R
@@ -1,4 +1,8 @@
-# Add this model to the list of models:
+# Add the human gestational PBTK model to the list of models:
+# 
+# Kapraun et al. "Evaluation of a Rapid, Multi-Chemical Human Gestational Dose Model"
+# in preparation
+
 
 #Analytic expression for steady-state plasma concentration.
 #model.list[["fetal_pbtk"]]$analytic.css.func <- "calc_analytic_css_fetal_pbtk" <function not yet developed
@@ -32,7 +36,8 @@ model.list[["fetal_pbtk"]]$alltissues=c(
   "spleen",
   "red blood cells",
   "thyroid",
-  "placenta")
+  "placenta",
+  "rest")
 
 # Which tissues from tissue.data are not lumped together when forming
 # the model: The fetal_pbtk model has liver, kidney, gut, and lung compartments
@@ -67,7 +72,9 @@ model.list[["fetal_pbtk"]]$param.names <- c(
   "kgutabs",                      
 # Maternal tissue partition coefficients:
   "Kadipose2pu",
+
   "Kgut2pu",
+
   "Kkidney2pu",
   "Kliver2pu",                    
   "Klung2pu",
@@ -96,6 +103,11 @@ model.list[["fetal_pbtk"]]$param.names <- c(
   "Vthyroidc",    
   "Vkidneyc",                     
   "Vgutc",                       
+
+
+
+
+
   "Vliverc",
   "Vlungc",                       
 # Tissue Densities:
@@ -202,31 +214,158 @@ model.list[["fetal_pbtk"]]$param.names <- c(
   "Qfthyroid_percent"
   )
 
-
 # This subset of R parameters are needed to initially parameterize the compiled
 # code for the solver (must match ORDER under "parameters" in C code, even if 
-# some items are omitted). String representations of the R version of names of
+# some items are omitted). 
+#
+# String representations of the R version of names of
 # the parameters are assigned to the C variable name in this scheme.
 model.list[["fetal_pbtk"]]$Rtosolvermap <- list(
   pre_pregnant_BW = "pre_pregnant_BW",
   Clmetabolismc = "Clmetabolismc",
+  Clmetabolism = "Clmetabolism",
   kgutabs = "kgutabs",
+  Kkidney2pu="Kkidney2pu",
+  Kliver2pu="Kliver2pu",
+  Kadipose2pu="Kadipose2pu",
+  Krest2pu="Krest2pu",
+  Klung2pu="Klung2pu",
+  Kgut2pu="Kgut2pu",
+  Krbc2pu="Krbc2pu",
+  Kthyroid2pu="Kthyroid2pu",
+  Kplacenta2pu="Kplacenta2pu",
+  Kfplacenta2pu="Kfplacenta2pu",
+  Kfkidney2pu="Kfkidney2pu",
+  Kfrest2pu="Kfrest2pu",
+  Kfthyroid2pu="Kfthyroid2pu",
+  Kfliver2pu="Kfliver2pu",
+  Kflung2pu="Kflung2pu",
+  Kfgut2pu="Kfgut2pu",
+  Kfrbc2pu="Kfrbc2pu",
+  Kfbrain2pu="Kfbrain2pu",
   Vgutc = "Vgutc",
+  Vgut = "Vgut",
   Vkidneyc = "Vkidneyc",
+  Vkidney = "Vkidney",
   Vliverc = "Vliverc",
+  Vliver = "Vliver",
   Vlungc = "Vlungc",
+  Vlung = "Vlung",
   Vthyroidc = "Vthyroidc",
+  Vthyroid = "Vthyroid",
   Fraction_unbound_plasma = "Funbound.plasma",
+  Fraction_unbound_plasma_fetus = "Fraction_unbound_plasma_fetus",
   gut_density = "gut_density",
   kidney_density = "kidney_density",
   liver_density = "liver_density",
   lung_density = "lung_density",
-  thyroid_density = "thyroid_density"
+  thyroid_density = "thyroid_density",
+  adipose_density = "adipose_density",
+  ffmx_density = "ffmx_density",
+  placenta_density = "placenta_density",
+  amnf_density = "amnf_density",                 
+  "brain_density = "brain_density",
+# Kapraun 2019 growth parameters:
+  BW_cubic_theta1 = "BW_cubic_theta1",
+  BW_cubic_theta2 = "BW_cubic_theta2",
+  BW_cubic_theta3 = "BW_cubic_theta3",
+  Wadipose_linear_theta0 = "Wadipose_linear_theta0",
+  Wadipose_linear_theta1 = "Wadipose_linear_theta1",
+  Wfkidney_gompertz_theta0 = "Wfkidney_gompertz_theta0",
+  Wfkidney_gompertz_theta1 = "Wfkidney_gompertz_theta1",
+  Wfkidney_gompertz_theta2 = "Wfkidney_gompertz_theta2",
+  Wfthyroid_gompertz_theta0 = "Wfthyroid_gompertz_theta0",
+  Wfthyroid_gompertz_theta1 = "Wfthyroid_gompertz_theta1",
+  Wfthyroid_gompertz_theta2 = "Wfthyroid_gompertz_theta2",
+  Wfliver_gompertz_theta0 = "Wfliver_gompertz_theta0",
+  Wfliver_gompertz_theta1 = "Wfliver_gompertz_theta1",
+  Wfliver_gompertz_theta2 = "Wfliver_gompertz_theta2",
+  Wfbrain_gompertz_theta0 = "Wfbrain_gompertz_theta0",
+  Wfbrain_gompertz_theta1 = "Wfbrain_gompertz_theta1",
+  Wfbrain_gompertz_theta2 = "Wfbrain_gompertz_theta2",
+  Wfgut_gompertz_theta0 = "Wfgut_gompertz_theta0",
+  Wfgut_gompertz_theta1 = "Wfgut_gompertz_theta1",
+  Wfgut_gompertz_theta2 = "Wfgut_gompertz_theta2",
+  Wflung_gompertz_theta0 = "Wflung_gompertz_theta0",
+  Wflung_gompertz_theta1 = "Wflung_gompertz_theta1",
+  Wflung_gompertz_theta2 = "Wflung_gompertz_theta2",
+  hematocrit_quadratic_theta0  = "hematocrit_quadratic_theta0",
+  hematocrit_quadratic_theta1 = "hematocrit_quadratic_theta1",
+  hematocrit_quadratic_theta2 = "hematocrit_quadratic_theta2",
+  fhematocrit_cubic_theta1  = "fhematocrit_cubic_theta1",
+  fhematocrit_cubic_theta2 = "fhematocrit_cubic_theta2",
+  fhematocrit_cubic_theta3 = "fhematocrit_cubic_theta3",
+  fBW_gompertz_theta0 = "fBW_gompertz_theta0",
+  fBW_gompertz_theta1 = "fBW_gompertz_theta1",
+  fBW_gompertz_theta2 = "fBW_gompertz_theta2",
+  Vplacenta_cubic_theta1 = "Vplacenta_cubic_theta1",
+  Vplacenta_cubic_theta2 = "Vplacenta_cubic_theta2",
+  Vplacenta_cubic_theta3 = "Vplacenta_cubic_theta3",
+  Vamnf_logistic_theta0 = "Vamnf_logistic_theta0",
+  Vamnf_logistic_theta1 = "Vamnf_logistic_theta1",
+  Vamnf_logistic_theta2 = "Vamnf_logistic_theta2",
+  Vplasma_mod_logistic_theta0 = "Vplasma_mod_logistic_theta0",
+  Vplasma_mod_logistic_theta1 = "Vplasma_mod_logistic_theta1",
+  Vplasma_mod_logistic_theta2 = "Vplasma_mod_logistic_theta2",
+  Vplasma_mod_logistic_theta3 = "Vplasma_mod_logistic_theta3",
+  venous_blood_fraction = "venous_blood_fraction",
+  arterial_blood_fraction = "arterial_blood_fraction",
+  fblood_weight_ratio = "fblood_weight_ratio",
+  Qcardiac_cubic_theta0 = "Qcardiac_cubic_theta0",
+  Qcardiac_cubic_theta1 = "Qcardiac_cubic_theta1",
+  Qcardiac_cubic_theta2 = "Qcardiac_cubic_theta2",
+  Qcardiac_cubic_theta3 = "Qcardiac_cubic_theta3",
+  term = "term",
+  Qgut_percent_initial = "Qgut_percent_initial",
+  Qgut_percent_termina = "Qgut_percent_terminal",
+  Qkidney_cubic_theta0 = "Qkidney_cubic_theta0",
+  Qkidney_cubic_theta1 = "Qkidney_cubic_theta1",
+  Qkidney_cubic_theta2 = "Qkidney_cubic_theta2",
+  Qkidney_cubic_theta3 = "Qkidney_cubic_theta3",
+  Qliver_percent_initial = "Qliver_percent_initial",
+  Qliver_percent_terminal = "Qliver_percent_terminal",
+  Qthyroid_percent_initial = "Qthyroid_percent_initial",
+  Qthyroid_percent_terminal = "Qthyroid_percent_terminal",
+  Qplacenta_linear_theta1 = "Qplacenta_linear_theta1",
+  Qadipose_percent_initial = "Qadipose_percent_initial",
+  Qadipose_percent_terminal = "Qadipose_percent_terminal",
+  Qgfr_quadratic_theta0 = "Qgfr_quadratic_theta0",
+  Qgfr_quadratic_theta1 = "Qgfr_quadratic_theta1",
+  Qgfr_quadratic_theta2 = "Qgfr_quadratic_theta2",
+  Qfrvtl_logistic_theta0 = "Qfrvtl_logistic_theta0",
+  Qfrvtl_logistic_theta1 = "Qfrvtl_logistic_theta1",
+  Qfrvtl_logistic_theta2 = "Qfrvtl_logistic_theta2",
+  Qflvtl_logistic_theta0 = "Qflvtl_logistic_theta0",
+  Qflvtl_logistic_theta1 = "Qflvtl_logistic_theta1",
+  Qflvtl_logistic_theta2 = "Qflvtl_logistic_theta2",
+  Qfda_logistic_theta0 = "Qfda_logistic_theta0",
+  Qfda_logistic_theta1= "Qfda_logistic_theta1",
+  Qfda_logistic_theta2 = "Qfda_logistic_theta2",
+  Qfplacenta_logistic_theta0= "Qfplacenta_logistic_theta0",
+  Qfplacenta_logistic_theta1= "Qfplacenta_logistic_theta1",
+  Qfplacenta_logistic_theta2 = "Qfplacenta_logistic_theta2",
+  Qfdv_gompertz_theta0  = "Qfdv_gompertz_theta0",
+  Qfdv_gompertz_theta =  "Qfdv_gompertz_theta1",
+  Qfdv_gompertz_theta2 =  "Qfdv_gompertz_theta2",
+  Qfnonplacental_percent =  "Qfnonplacental_percent",
+  Qfgut_percent = "Qfgut_percent",
+  Qfkidney_percent = "Qfkidney_percent",
+  Qfbrain_percent = "Qfbrain_percent",
+  Qbrain_percent = "Qbrain_percent",
+  Qkidney_percent = "Qkidney_percent",
+  Qgut_percent = "Qgut_percent",
+  Qfthyroid_percen  = "Qfthyroid_percen",
+  Qfthyroid_percen = "Qfthyroid_percent"
 )
 
 # This function translates the R model parameters into the compiled model
 # parameters:
 model.list[["fetal_pbtk"]]$compiled.parameters.init <- "getParmsfetal_pbtk"
+
+
+# This needs to be a global variable so that R CMD check --as-cran can test
+# the code (the HTTK package does not use this):
+compiled_parameters_init <- "getParmsfetal_pbtk"
 
 # This is the ORDERED full list of parameters used by the compiled code to 
 # calculate the derivative of the system of equations describing the model.
@@ -522,6 +661,17 @@ model.list[["fetal_pbtk"]]$dosing.params <- c("daily.dose",
                                         "doses.per.day",
                                         "dosing.matrix")
 model.list[["fetal_pbtk"]]$routes <- c("oral","iv")
+
+
+
+
+
+
+
+
+
+
+
 # We need to know which compartment gets the dose 
 model.list[["fetal_pbtk"]]$dose.variable <- list(oral="Agutlumen",
                                            iv="Aven")
@@ -530,6 +680,8 @@ model.list[["fetal_pbtk"]]$dose.variable <- list(oral="Agutlumen",
 #or "multiply" to change the value to C1 <- C1*dose
 model.list[["fetal_pbtk"]]$dose.type <- list(oral="add",
                                        iv="add")
+
+
 
 # ORDERED LIST of state variables (must match Model variables: 
 # States in C code, each of which is associated with a differential equation),
@@ -561,6 +713,21 @@ model.list[["pbtk"]]$state.vars <- c(
   "Afkidney",
   "Afbrain"
   ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
        
 #Parameters needed to make a prediction (this is used by get_cheminfo):
 model.list[["fetal_pbtk"]]$required.params <- c(
@@ -570,7 +737,39 @@ model.list[["fetal_pbtk"]]$required.params <- c(
   "pKa_Donor",
   "pKa_Accept",
   "MW"
-)
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Do we need to recalculate partition coefficients when doing Monte Carlo?
 model.list[["pbtk"]]$calcpc <- TRUE
@@ -581,3 +780,6 @@ model.list[["pbtk"]]$firstpass <- FALSE
 
 # Do we ignore the Fups where the value was below the limit of detection?
 model.list[["fetal_pbtk"]]$exclude.fup.zero <- T
+
+
+
