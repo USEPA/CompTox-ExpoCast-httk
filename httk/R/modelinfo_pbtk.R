74445882f03f4c89698e2b219db218963dc5a150
Sarah Davidson <davidson.sarah.e@epa.gov>
2022-01-31 12:21:04 -0500

    HTTK-63 update; re-instating the volatile compound filtering using logHenry from HTTK-6 & the chemical class filtering from HTTK-7

--- httk/R/modelinfo_pbtk.R
+++ httk/R/modelinfo_pbtk.R
@@ -346,3 +346,9 @@ model.list[["pbtk"]]$exclude.fup.zero <- T
 
 # These are the parameter names needed to describe steady-state dosing:
 model.list[["pbtk"]]$css.dosing.params <- c("hourly.dose")
+
+# Filter out volatile compounds with Henry's Law Constant Threshold
+model.list[["pbtk"]]$log.henry.threshold <- c(-4.5)
+
+# Filter out compounds belonging to select chemical classes
+model.list[["pbtk"]]$chem.class.filt <- c("PFAS")
