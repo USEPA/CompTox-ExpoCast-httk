c6e980334e7c68239e657e80a8948a366ef87a79
John Wambaugh <wambaugh.john@epa.gov>
2022-02-02 12:53:05 -0500

    Revert "Update armitage.R"

--- R/armitage.R
+++ R/armitage.R
@@ -112,9 +112,6 @@ armitage_estimate_sarea <- function(tcdata = NA, # optionally supply columns v_w
 #' @param this.celldensity Cell density kg/L, g/mL
 #' @param this.cellmass Mass per cell, ng/cell
 #' @param this.f_oc 1, everything assumed to be like proteins
-#' @param this.conc_ser_alb Mass concentration of albumin in serum, g/L
-#' @param this.conc_ser_lip Mass concentration of lipids in serum, g/L
-#' @param this.Vdom The volume of dissolved organic matter (DOM), ml
 #' @return tcdata
 #' @author Greg Honda
 #' @references Armitage, J. M.; Wania, F.; Arnot, J. A. Environ. Sci. Technol. 2014, 48, 9770-9779. https://doi.org/10.1021/es501955g
@@ -129,7 +126,7 @@ armitage_estimate_sarea <- function(tcdata = NA, # optionally supply columns v_w
 armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
                           nomconc.vector = 1, # nominal concentration vector (e.g. apparent AC50 values)
                           this.well_number = 384,
-                          this.FBSf = NA_real_, # Must be set if not in tcdata, this is the most senstive parameter in the model.
+                          this.FBSf = NA_real_, # Must be set if not in tcdata, this is the most senstive parameter in the model
                           tcdata = NA, # A data.table with casrn, ac50, and well_number or all of sarea, v_total, and v_working
                           this.sarea = NA_real_,
                           this.v_total = NA_real_,
@@ -148,10 +145,7 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
                           this.csalt = 0.15, # ionic strength of buffer, mol/L
                           this.celldensity=1, # kg/L g/mL  mg/uL
                           this.cellmass = 3, #ng/cell
-                          this.f_oc = 1, # everything assumed to be like proteins
-                          this.conc_ser_alb = 24, # g/L mass concentration of albumin in serum.
-                          this.conc_ser_lip = 1.9, # g/L mass concentration of lipids in serum.
-                          this.Vdom = 0 # ml the volume of dissolved organic matter (DOM)
+                          this.f_oc = 1 # everything assumed to be like proteins
 ){
   # this.Tsys <- 37
   # this.Tref <- 298.15
@@ -168,8 +162,8 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
   # this.celldensity<-1 # kg/L g/mL  mg/uL
   # this.cellmass <- 3 #ng/cell
   # this.f_oc <- 1 # everything assumed to be like proteins
-  
-  
+
+
   #R CMD CHECK throws notes about "no visible binding for global variable", for
   #each time a data.table column name is used without quotes. To appease R CMD
   #CHECK, a variable has to be created for each of these column names and set to
@@ -187,7 +181,7 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
   ccells<-eta_free <- cfree.invitro <- nomconc <- well_number <- NULL
   logHenry <- logWSol <- NULL
   #End R CMD CHECK appeasement.
-  
+
   if(all(is.na(tcdata))){
     tcdata <- data.table(casrn = casrn.vector,
                          nomconc = nomconc.vector,
@@ -219,14 +213,12 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
     
     if(any(is.na(tcdata[missing.rows, well_number]))){
       print(paste0("Either well_number or geometry must be defined for rows: ", 
-                   paste(which(tcdata[, is.na(sarea) & is.na(well_number)]),collapse = ",")))
+                  paste(which(tcdata[, is.na(sarea) & is.na(well_number)]),collapse = ",")))
       stop()
     }else{
       temp <- armitage_estimate_sarea(tcdata[missing.rows,])
       tcdata[missing.rows,"sarea"] <- temp[,"sarea"]
-      if(any(is.na(tcdata[missing.rows,"v_total"]))){
-        tcdata[missing.rows,"v_total"] <- temp[,"v_total"]
-      }
+      tcdata[missing.rows,"v_total"] <- temp[,"v_total"]
       tcdata[missing.rows,"v_working"] <- temp[,"v_working"]
       tcdata[missing.rows,"cell_yield"] <- temp[,"cell_yield"]
     }
@@ -241,85 +233,79 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
                                 chem.cas = casrn)]
   }
   tcdata[, "gkaw" := logHenry - log10(298.15*8.2057338e-5)] # log10 atm-m3/mol to (mol/m3)/(mol/m3)
-  
+
   manual.input.list <- list(Tsys=this.Tsys, Tref=this.Tref,
-                            option.kbsa2=this.option.kbsa2, option.swat2=this.option.swat2,
-                            FBSf=this.FBSf, pseudooct=this.pseudooct, memblip=this.memblip,
-                            nlom=this.nlom, P_nlom=this.P_nlom, P_dom=this.P_dom, P_cells=this.P_cells,
-                            csalt=this.csalt, celldensity=this.celldensity, cellmass=this.cellmass, f_oc=this.f_oc,
-                            conc_ser_alb = this.conc_ser_alb, conc_ser_lip = this.conc_ser_lip, Vdom = this.Vdom)
-  
+                                option.kbsa2=this.option.kbsa2, option.swat2=this.option.swat2,
+                                FBSf=this.FBSf, pseudooct=this.pseudooct, memblip=this.memblip,
+                                nlom=this.nlom, P_nlom=this.P_nlom, P_dom=this.P_dom, P_cells=this.P_cells,
+                                csalt=this.csalt, celldensity=this.celldensity, cellmass=this.cellmass, f_oc=this.f_oc)
+
   check.list <- c("dsm","duow","duaw","dumw",
                   "gkmw","gkcw","gkbsa","gkpl","ksalt")
-  
+
   req.list <- c("Tsys","Tref","option.kbsa2","option.swat2",
-                "FBSf","pseudooct","memblip","nlom","P_nlom","P_dom","P_cells",
-                "csalt","celldensity","cellmass","f_oc","conc_ser_alb","conc_ser_lip","Vdom")
+                 "FBSf","pseudooct","memblip","nlom","P_nlom","P_dom","P_cells",
+                 "csalt","celldensity","cellmass","f_oc")
   if(!all(check.list%in%names(tcdata))){
-    tcdata[,check.list[!(check.list %in% names(tcdata))]] <- as.double(NA)}
-  
+  tcdata[,check.list[!(check.list %in% names(tcdata))]] <- as.double(NA)}
+
   if(!all(req.list%in%names(tcdata))){
-    tcdata[,req.list[!(req.list %in% names(tcdata))]] <- manual.input.list[!(names(manual.input.list) %in% names(tcdata))]}
-  
-  R <- 8.3144621 # J/(mol*K)
-  
+  tcdata[,req.list[!(req.list %in% names(tcdata))]] <- manual.input.list[!(names(manual.input.list) %in% names(tcdata))]}
+
+  R <- 8.3144
+
   tcdata[,cellwat := 1-(pseudooct+memblip+nlom)] %>%
     .[,Tsys:=Tsys+273.15] %>%
     .[,Tcor:=((1/Tsys)-(1/Tref))/(2.303*R)]
-  
-  
-  tcdata[,Vbm:=v_working/1e6] %>% # uL to L; the volume of bulk medium
-    .[,Vwell:=v_total/1e6] %>% # uL to L; the volume of well
-    #.[,Vair:=Vwell-Vm] %>%
-    .[,Vcells:=cell_yield*(cellmass/1e6)/celldensity/1e6] %>% # cell*(ng/cell)*(1mg/1e6ng)/(mg/uL)*(1uL/L); the volume of cells.
-    .[,Vair:=Vwell-Vbm-Vcells] %>%  # the volume of head space
-    .[,Valb:=Vbm*FBSf*0.733*conc_ser_alb/1000] %>% # the volume of serum albumin
-    .[,Vslip:=Vbm*FBSf*conc_ser_lip/1000] %>% # the volume of serum lipids
-    #.[,Vdom:=0] %>%
-    .[,Vdom:=Vdom/1e6] %>% # uL to L; the volume of Dissolved Organic Matter (DOM)
-    .[,Vm:=Vbm-Valb-Vslip-Vdom] # the volume of medium
-    
-  
-  tcdata[is.na(dsm),dsm:=56.5] %>% #J/(mol*K) # Walden's rule
-    .[is.na(duow),duow:=-20000] %>% # see SI EQC model - in vitro tox test July 2014.xlsm
-    .[is.na(duaw),duaw:=60000] %>% # see SI EQC model - in vitro tox test July 2014.xlsm
+
+
+  tcdata[,Vm:=v_working/1e6] %>% # uL to L
+      .[,Vwell:=v_total/1e6] %>% # uL to L
+      .[,Vair:=Vwell-Vm] %>%
+      .[,Vcells:=cell_yield*(cellmass/1e6)/celldensity/1e6] %>% # cell*(ng/cell)*(1mg/1e6ng)/(mg/uL)*(1uL/L)
+      .[,Valb:=Vm*FBSf*0.733*24/1000] %>%
+      .[,Vslip:=Vm*FBSf*1.9/1000] %>%
+      .[,Vdom:=0]
+
+  tcdata[is.na(dsm),dsm:=56.5] %>%
+    .[is.na(duow),duow:=-20000] %>%
+    .[is.na(duaw),duaw:=60000] %>%
     .[is.na(dumw),dumw:=duow] %>%
     .[,MP:= MP+273.15] %>%
-    #.[,F_ratio:=exp(-(dsm/R)*(MP/Tsys))] %>% 
-    .[,F_ratio:=10^(0.01*(Tsys-MP))] %>% 
+    .[,F_ratio:=exp(-(dsm/R)*(MP/Tsys))] %>% #Walden's rule
     .[MP<=Tsys,F_ratio:=1]
-  
+
   tcdata[,gs1.GSE:=0.5-gkow] %>%
     .[,gs1.GSE:=gs1.GSE-(-1*duow)*Tcor] %>%
     .[,s1.GSE:=10^gs1.GSE] %>%
     .[MP>298.15,gss.GSE:=0.5-0.01*((MP-273.15)-25)-gkow] %>%
     .[MP>298.15,gss.GSE:=gss.GSE-(-1*duow)*Tcor] %>%
     .[MP>298.15,ss.GSE:=10^gss.GSE] %>%
-    .[is.na(gkmw),gkmw:=1.01*gkow + 0.12] %>% 
+    .[is.na(gkmw),gkmw:=1.01*gkow + 0.12] %>%
     .[,gkmw:=gkmw-dumw*Tcor] %>%
     .[,kmw:=10^gkmw] %>%
-    .[,gkow:=gkow-duow*Tcor] %>%
+    .[,gkow:=gkow-duow*Tcor] %>% # check this
     .[,kow:=10^gkow] %>%
     .[,gkaw:=gkaw-duaw*Tcor] %>%
     .[,kaw := 10^gkaw] %>%
     .[,gswat:=gswat-(-1*duow)*Tcor] %>%
     .[,swat:=10^gswat]
-  
-  tcdata[is.na(gkpl),gkpl:=0.97*gkow-6.94] %>% 
+
+  tcdata[is.na(gkpl),gkpl:=0.97*gkow-6.94] %>%
     .[,kpl:=10^gkpl] %>%
     .[!(is.na(gkcw)),gkcw:=gkcw-duow*Tcor] %>%
     .[is.na(gkcw),gkcw:=log10(P_cells*pseudooct*kow + memblip*kmw +
                                 P_nlom*nlom*kow + cellwat)] %>%
     .[,kcw:=10^gkcw]
-  
+
   tcdata[!(is.na(gkbsa)),gkbsa:=gkbsa-duow*Tcor] %>%
     .[!(is.na(gkbsa)),kbsa:=10^gkbsa]
-  
+
   tcdata[option.kbsa2==T & is.na(gkbsa) & gkaw<4.5, kbsa:=10^(1.08*gkow-0.7)] %>%
     .[option.kbsa2==T & is.na(gkbsa) & gkaw>=4.5, kbsa:=10^(0.37*gkow+2.56)]
-  
+
   tcdata[option.kbsa2==F & is.na(gkbsa),kbsa:=10^(0.71*gkow+0.42)]
-  
+
   tcdata[is.na(ksalt),ksalt:=0.04*gkow+0.114] %>%
     .[,swat:=swat*10^(-1*ksalt*csalt)] %>%
     .[,s1.GSE:=s1.GSE*10^(-1*ksalt*csalt)] %>%
@@ -329,29 +315,29 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
     .[,kaw:=kaw/(10^(-1*ksalt*csalt))] %>%
     .[,kcw:=kcw/(10^(-1*ksalt*csalt))] %>%
     .[,kbsa:=kbsa/(10^(-1*ksalt*csalt))]
-  
+
   tcdata[option.swat2==T & MP>298.15,swat:=ss.GSE] %>%
     .[option.swat2==T & MP>298.15,swat_L:=s1.GSE] %>%  # double check this
     .[option.swat2==T & MP<=298.15,swat:=s1.GSE] %>%
     .[option.swat2==T & MP<=298.15,swat_L:=s1.GSE]
-  
+
   tcdata[,soct_L:=kow*swat_L] %>%
     .[,scell_L:=kcw*swat_L]
-  
+
   tcdata[,nomconc := nomconc/1e6] %>% # umol/L to mol/L
     .[,cinit:= nomconc] %>%
-    .[,mtot:= nomconc*Vbm] %>%
+    .[,mtot:= nomconc*Vm] %>% # total moles
     .[,cwat:=mtot/(kaw*Vair + Vm + kbsa*Valb +
-                     P_cells*kow*Vslip + kow*P_dom*f_oc*Vdom + kcw*Vcells +
+                     P_cells*kow*Vslip + P_dom*f_oc*Vdom + kcw*Vcells +
                      1000*kpl*sarea)] %>%
     .[cwat>swat,cwat_s:=swat] %>%
     .[cwat>swat,csat:=1] %>%
     .[cwat<=swat,cwat_s:=cwat] %>%
     .[cwat<=swat,csat:=0] %>%
     .[,activity:=cwat_s/swat_L]
-  
+
   tcdata[,c("cair","calb","cslip","cdom","ccells")] <- 0
-  
+
   tcdata[Vair>0,cair:=kaw*cwat_s] %>%
     .[Valb>0,calb:=kbsa*cwat_s] %>%
     .[Vslip>0,cslip:=kow*cwat_s*P_cells] %>%
@@ -383,3 +369,4 @@ armitage_eval <- function(casrn.vector = NA_character_, # vector of CAS numbers
   #output mass (mwat_s etc.) in mols
   #output mol fraction xbsa etc.
 }
+
