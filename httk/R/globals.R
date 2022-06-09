318d43382c3ea49064b81c39325f83c26577d411
cring <Ring.Caroline@epa.gov>
2022-06-09 17:34:43 -0400

    Moving declaration of global httkpop variables to globals.R

--- httk/R/globals.R
+++ httk/R/globals.R
@@ -1,2 +1,16 @@
 #File for declaring global data.table objects for CRAN binding check
 utils::globalVariables(c('tissue.data','physiology.data'))
+
+utils::globalVaiables(c("bmiage",
+                        "mcnally_dt",
+                        "nhanes_mec_svy",
+                        "wfl",
+                        "DT_age",
+                        "kde_centers",
+                        "hw_H",
+                        "hct_h",
+                        "scr_h",
+                        "height_spline",
+                        "weight_spline",
+                        "hct_spline",
+                        "scr_spline"))
