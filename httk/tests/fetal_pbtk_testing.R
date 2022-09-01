13d7bad94a0cb49abd04ade5d3a8bae0e59a2b9d
Wambaugh <Wambaugh.John@epa.gov>
2022-09-01 17:02:43 -0400

    HTTK-94 updated test save files

--- httk/tests/fetal_pbtk_testing.R
+++ httk/tests/fetal_pbtk_testing.R
@@ -1,9 +1,9 @@
-#R CMD BATCH --no-timing --no-restore --no-save solve_fetal_crude_test.R solve_fetal_crude_test.Rout
+#R CMD BATCH --no-timing --no-restore --no-save fetal_pbtk_testing.R fetal_pbtk_testing.Rout
 library(httk)
 options(warn=-1)
 
 head(solve_fetal_pbtk(chem.name = 'bisphenol a', daily.dose = 1,
-                                             doses.per.day = 3, plots = TRUE))
+                                             doses.per.day = 3))
 head(solve_fetal_pbtk(chem.cas="80-05-7"))
 head(solve_fetal_pbtk(parameters=parameterize_fetal_pbtk(chem.cas="80-05-7")))
 
