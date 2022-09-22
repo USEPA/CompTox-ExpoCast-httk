77cd17f2ced03dd650f1e4dd097af97694074d4e
jwambaug <wambaugh.john@epa.gov>
2022-09-22 12:46:09 -0400

    Fixed problems for CRAN

--- httk/tests/solve_gas_test.R
+++ httk/tests/solve_gas_test.R
@@ -1,10 +1,11 @@
 #R CMD BATCH --no-timing --no-restore --no-save solve_gas_test.R solve_gas_test.Rout
 library(httk)
 
-signif(head(solve_gas_pbtk(chem.name="pyrene")),3)
-signif(head(solve_gas_pbtk(chem.cas="129-00-0")),3)
+signif(head(solve_gas_pbtk(chem.name="pyrene",times=c(0,0.1,0.05)))[,-11],3)
+signif(head(solve_gas_pbtk(chem.cas="129-00-0",times=c(0,0.1,0.05)))[,-11],3)
 signif(head(solve_gas_pbtk(
-  parameters=parameterize_gas_pbtk(chem.cas="129-00-0"))),3)
+  parameters=parameterize_gas_pbtk(chem.cas="129-00-0"),
+  times=c(0,0.1,0.05)))[,-11],3)
 
 parameterize_gas_pbtk(chem.name="styrene")
 
