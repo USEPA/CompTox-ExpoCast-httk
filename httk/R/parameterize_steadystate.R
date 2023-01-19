ae19eee1527c91b1d114e39589f89baf0f7c9103
Wambaugh <Wambaugh.John@epa.gov>
2023-01-19 10:58:09 -0500

    started new vignette

--- httk/R/parameterize_steadystate.R
+++ httk/R/parameterize_steadystate.R
@@ -201,9 +201,11 @@ parameterize_steadystate <- function(
                       silent=TRUE)
     warning(paste(species,"coerced to Human for metabolic clerance data."))
   }
-  if (is(Clint.db,"try-error")) 
-    stop("Missing metabolic clearance data for given species.\n\
+  if (is(Clint.db,"try-error") & tolower(species=="human")) 
+    stop("Missing metabolic clearance data for requested species.\n\
 Set default.to.human to true to substitute human value.")
+  else if (is(Clint.db,"try-error") stop("
+Missing metabolic clearance data for requested species.")
 # Check if clintis a point value or a distribution, if a distribution, 
 #use the median:
   if (nchar(Clint.db) - nchar(gsub(",","",Clint.db))==3) 
