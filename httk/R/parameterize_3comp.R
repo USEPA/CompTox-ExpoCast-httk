bd7a5821a9f466d90867858dfa10affebe6c6586
Sarah Davidson-Fritz <davidsonfritz.sarah@epa.gov>
2023-09-13 11:43:08 -0400

    update the parameterize_3comp arguments to include Caco2.option for argument passed by calc_analytic_css_3comp - HTTK-163 & HTTK-171

--- httk/R/parameterize_3comp.R
+++ httk/R/parameterize_3comp.R
@@ -45,6 +45,14 @@
 #' @param minimum.Funbound.plasma Monte Carlo draws less than this value are set 
 #' equal to this value (default is 0.0001 -- half the lowest measured Fup in our
 #' dataset).
+#' 
+#' @param Caco2.options A list of options to use when working with Caco2 apical to
+#' basolateral data \code{Caco2.Pab}, default is Caco2.options = list(Caco2.default = 2,
+#' Caco2.Fabs = TRUE, Caco2.Fgut = TRUE, overwrite.invivo = FALSE, keepit100 = FALSE). Caco2.default sets the default value for 
+#' Caco2.Pab if Caco2.Pab is unavailable. Caco2.Fabs = TRUE uses Caco2.Pab to calculate
+#' fabs.oral, otherwise fabs.oral = \code{Fabs}. Caco2.Fgut = TRUE uses Caco2.Pab to calculate 
+#' fgut.oral, otherwise fgut.oral = \code{Fgut}. overwrite.invivo = TRUE overwrites Fabs and Fgut in vivo values from literature with 
+#' Caco2 derived values if available. keepit100 = TRUE overwrites Fabs and Fgut with 1 (i.e. 100 percent) regardless of other settings.
 #'
 #' @return 
 #' \item{BW}{Body Weight, kg.} 
@@ -129,7 +137,8 @@ parameterize_3comp<- function(
                        regression = TRUE,
                        suppress.messages = FALSE,
                        restrictive.clearance = TRUE,
-                       minimum.Funbound.plasma = 0.0001)
+                       minimum.Funbound.plasma = 0.0001,
+                       Caco2.options = NULL)
 {
   parms <- parameterize_pbtk(
              chem.cas = chem.cas,
@@ -147,7 +156,8 @@ parameterize_3comp<- function(
              regression = regression,
              suppress.messages = suppress.messages,
              restrictive.clearance = restrictive.clearance,
-             minimum.Funbound.plasma = minimum.Funbound.plasma)
+             minimum.Funbound.plasma = minimum.Funbound.plasma,
+             Caco2.options = Caco2.options)
                               
   parms$Qkidneyf <- parms$Vvenc <- parms$Vartc <- NULL
  
