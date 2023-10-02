3d787f2cea17ae1dcfd8cdccac8b99fa94859721
Wambaugh <Wambaugh.John@epa.gov>
2023-10-02 17:23:43 -0400

    Now can specify the state of each compartment for proper unit conversion HTTK-180

--- httk/tests/1comp_test.R
+++ httk/tests/1comp_test.R
@@ -1,4 +1,7 @@
 #R CMD BATCH --no-timing --no-restore --no-save 1comp_test.R 1comp_test.Rout
+# Get rid of anything in the workspace:
+rm(list=ls()) 
+
 library(httk)
 
 #calc_analytic_css(chem.name="bisphenol a",model="1compartment")
