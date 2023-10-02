259ec79f8953d612a24cb2bba0ac73e35e0c80c7
Wambaugh <Wambaugh.John@epa.gov>
2023-10-02 17:28:15 -0400

    Updating tests

--- httk/tests/3comp_test.R
+++ httk/tests/3comp_test.R
@@ -1,4 +1,5 @@
-#R CMD BATCH --no-timing --no-restore --no-save 3comp_test.R 3comp_test.Rout
+# R CMD BATCH --no-timing --no-restore --no-save 3comp_test.R 3comp_test.Rout
+
 # Get rid of anything in the workspace:
 rm(list=ls()) 
 
