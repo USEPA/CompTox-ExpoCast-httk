54ad52fe267f72b062a3c34a05dbf51cd330eaef
Wambaugh <Wambaugh.John@epa.gov>
2024-03-19 15:47:10 -0400

    Fixes to state equal all and passes ascran

--- httk/tests/solve_gas_test.R
+++ httk/tests/solve_gas_test.R
@@ -5,7 +5,7 @@ rm(list=ls())
 
 library(httk)
 
-# The following arguments were added: method = "losde",mf = 10.
+# The following arguments were added: method = "lsode",mf = 10.
 # Rationale: Following is required for the same results on various OS's due to
 #   precision differences.
 head(solve_gas_pbtk(chem.name="pyrene",times=c(0,0.1,0.05),
