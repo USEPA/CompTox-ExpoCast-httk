5f77fa5ce0c4e71c5ece2401d4c20b2d093cb4bf
Wambaugh <Wambaugh.John@epa.gov>
2024-07-16 13:04:41 -0400

    HTTK-145 Added new function check_model to make sure chemical selected is listed in get_cheminfo

---
+++ httk/R/check_model.R
@@ -0,0 +1,113 @@
+#' Check for sufficient model parameters
+#' 
+#' @description
+#' This function halt model evaluation if not all the needed parameters
+#' (as specified in the modelinfo_[MODEL].r file) are available. The function uses
+#' \code{\link{get_cheminfo}}, so if the chemical has been checked against that
+#' function already then evaluation should proceed as expected. If you do not
+#' have the parameters you need and are using a non-human
+#' species try default.to.human = TRUE (there are many more values for human than
+#' any other species). If working in human, try first using
+#' \code{\link{load_dawson2021}}, \code{\link{load_sipes2017}}, or
+#' \code{\link{load_pradeep2020}}.
+#'
+#' @param chem.cas Chemical Abstract Services Registry Number (CAS-RN) -- if
+#'  parameters is not specified then the chemical must be identified by either
+#'  CAS, name, or DTXISD
+#' 
+#' @param chem.name Chemical name (spaces and capitalization ignored) --  if
+#'  parameters is not specified then the chemical must be identified by either
+#'  CAS, name, or DTXISD
+#' 
+#' @param dtxsid EPA's DSSTox Structure ID (\url{https://comptox.epa.gov/dashboard})  
+#'  -- if parameters is not specified then the chemical must be identified by 
+#' either CAS, name, or DTXSIDs
+#' 
+#' @param species Species desired (either "Rat", "Rabbit", "Dog", "Mouse", or
+#' default "Human").
+#' 
+#' @param default.to.human Substitutes missing fraction of unbound plasma with
+#' human values if true.
+#' 
+#' @return Stops code from running if all parameters needed for model
+#' are not available, otherwise does nothing.
+#'
+#' @author john Wambaugh
+#'
+#' @keywords Parameter 
+#'
+#' @seealso \code{\link{get_cheminfo}}
+#'
+#' @examples
+#' # Should work:
+#' check_model(chem.cas="80-05-7",model="pbtk",species="rat")
+#' # Should work:
+#' check_model(chem.cas="80-05-7",model="pbtk",species="human")
+#' # Should throw an error:
+#' check_model(chem.cas="80-05-1",model="pbtk",species="human")
+#' # Should throw an error:
+#' check_model(chem.cas="80-05-1",model="pbtk",species="rat")
+check_model <- function(chem.name=NULL,
+                        chem.cas=NULL,
+                        dtxsid=NULL,
+                        model=NULL,
+                        species=NULL,
+                        default.to.human=FALSE)
+{
+  good.chems <- get_cheminfo(info=c("Compound",
+                                    "CAS",
+                                    "DTXSID"),
+                             model=model,
+                             species=species,
+                             suppress.messages=TRUE)
+  if (tolower(species) != "human" & default.to.human)
+  {
+    good.chems <- rbind(good.chems,
+                        get_cheminfo(info=c("Compound",
+                                     "CAS",
+                                     "DTXSID"),
+                                     model=model,
+                                     species="Human",
+                                     suppress.messages=TRUE)
+                        )       
+  }
+                                                
+  chem.present <- FALSE
+  if (!is.null(chem.cas))
+  { 
+    if (chem.cas %in% good.chems[,"CAS"]) chem.present <- TRUE
+  }
+  if (!is.null(dtxsid))
+  {
+     if (dtxsid %in% good.chems[,"DTXSID"]) chem.present <- TRUE
+  }
+  if (!is.null(chem.name))
+  {
+    if (chem.name %in% good.chems[,"Compound"]) chem.present <- TRUE
+  }  
+  
+  if (!(chem.present)) 
+  {
+    if (tolower(species) != "human" &
+        !default.to.human) stop(paste0("Chemical CAS: ",
+      chem.cas,
+      ", DTXSID: ",
+      dtxsid,
+      ", named: ",
+      chem.name,
+      " either has insufficient parameters or is outside the domain for model ",
+      model,
+      ". Try setting default.to.human=TRUE"
+      ))
+    else stop(paste0("Chemical CAS: ",
+      chem.cas,
+      ", DTXSID: ",
+      dtxsid,
+      ", named: ",
+      chem.name,
+      " either has insufficient parameters or is outside the domain for model ",
+      model,
+      "."
+      ))
+  }
+}
