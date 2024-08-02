2cb390983984f8286264373d037d1ccd9976d770
Wambaugh <Wambaugh.John@epa.gov>
2024-08-02 11:34:35 -0400

    CLean up roxygen

--- httk/R/calc_analytic_css_3compss2.R
+++ httk/R/calc_analytic_css_3compss2.R
@@ -1,49 +1,60 @@
-#'Calculate the steady state concentration for the three compartment
-#'steady-state model with exhalation
+#' Calculate the steady state concentration for the three compartment
+#' steady-state model with exhalation
 #'
-#'This function calculates the analytic steady state plasma or venous blood 
-#'concentrations as a result of infusion dosing.
+#' This function calculates the analytic steady state plasma or venous blood 
+#' concentrations as a result of infusion dosing.
 #'
-#'@param chem.name Either the chemical name, CAS number, or the parameters must 
+#' @param chem.name Either the chemical name, CAS number, or the parameters must 
 #' be specified.
-#'@param chem.cas Either the chemical name, CAS number, or the parameters must 
+#'
+#' @param chem.cas Either the chemical name, CAS number, or the parameters must 
 #' be specified.
+#'
 #' @param dtxsid EPA's 'DSSTox Structure ID (\url{https://comptox.epa.gov/dashboard})   
 #' the chemical must be identified by either CAS, name, or DTXSIDs
-#'@param parameters Chemical parameters from parameterize_pbtk (for model = 
+#'
+#' @param parameters Chemical parameters from parameterize_pbtk (for model = 
 #' 'pbtk'), parameterize_3comp (for model = '3compartment), 
 #' parameterize_1comp(for model = '1compartment') or parameterize_steadystate 
 #' (for model = '3compartmentss'), overrides chem.name and chem.cas.
-#'@param hourly.dose Hourly dose rate mg/kg BW/h.
-#'@param concentration Desired concentration type, 'blood' or default 'plasma'.
-#'@param suppress.messages Whether or not the output message is suppressed.
-#'@param recalc.blood2plasma Recalculates the ratio of the amount of chemical 
+#'
+#' @param hourly.dose Hourly dose rate mg/kg BW/h.
+#'
+#' @param concentration Desired concentration type, 'blood' or default 'plasma'.
+#'
+#' @param suppress.messages Whether or not the output message is suppressed.
+#'
+#' @param recalc.blood2plasma Recalculates the ratio of the amount of chemical 
 #' in the blood to plasma using the input parameters. Use this if you have 
 #' 'altered hematocrit, Funbound.plasma, or Krbc2pu.
-#'@param tissue Desired tissue concentration (defaults to whole body 
+#'
+#' @param tissue Desired tissue concentration (defaults to whole body 
 #'concentration.)
-#'@param restrictive.clearance If TRUE (default), then only the fraction of
+#'
+#' @param restrictive.clearance If TRUE (default), then only the fraction of
 #' chemical not bound to protein is available for metabolism in the liver. If 
 #' FALSE, then all chemical in the liver is metabolized (faster metabolism due
 #' to rapid off-binding). 
-#'@param bioactive.free.invivo If FALSE (default), then the total concentration is treated
+#'
+#' @param bioactive.free.invivo If FALSE (default), then the total concentration is treated
 #' as bioactive in vivo. If TRUE, the the unbound (free) plasma concentration is treated as 
 #' bioactive in vivo. Only works with tissue = NULL in current implementation.
-#'@param ... Additional parameters passed to parameterize function if 
-#'parameters is NULL.
+#'
+#' @param ... Additional parameters passed to parameterize function if 
+#' parameters is NULL.
 #'  
-#'@return Steady state plasma concentration in mg/L units
+#' @return Steady state plasma concentration in mg/L units
 #'
 #' @seealso \code{\link{calc_analytic_css}}
 #'
 #' @seealso \code{\link{parameterize_steadystate}}
 #'
-#'@author ohn Wambaugh
+#' @author ohn Wambaugh
 #'
 #' @references Pearce, Robert G., et al. "Httk: R package for high-throughput
 #' toxicokinetics." Journal of statistical software 79.4 (2017): 1.
 #'
-#'@keywords 3compss2
+#' @keywords 3compss2
 #'
 #' @export calc_analytic_css_3compss2
 calc_analytic_css_3compss2 <- function(chem.name=NULL,
