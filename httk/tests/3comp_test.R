2893eb3fbb4d07b2bf540963346487cc2ef0d787
Wambaugh <Wambaugh.John@epa.gov>
2024-08-27 12:29:30 -0400

    Trying to get parameters listed consistently in tests

--- httk/tests/3comp_test.R
+++ httk/tests/3comp_test.R
@@ -53,5 +53,7 @@ initial_nondefault_dose_intestine = out_nondefault_dosing[2,"Aintestine"]
 head(initial_nondefault_dose_intestine)
 
 p <- parameterize_3comp(chem.name="Aminopterin")[sort(names(parameterize_3comp(chem.name="Aminopterin")))]
-for (this.param in names(p)) cat(paste(this.param,": ",p[[this.param]],"\r\n",sep=""))
+# Try to standardize order of variable names
+for (this.param in names(p)[order(toupper(names(p)))]) cat(paste(this.param,": ",p[[this.param]],"\r\n",sep=""))
+
 quit("no")
