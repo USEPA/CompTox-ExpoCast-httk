9e7f7ecb28aa6c383d5aac125cb8466fdd312095
Sarah Davidson-Fritz <davidsonfritz.sarah@epa.gov>
2025-02-25 16:49:57 -0500

    update the loading functions to utilize the generalized httk chem subset function

--- httk/R/load_pradeep2020.R
+++ httk/R/load_pradeep2020.R
@@ -76,28 +76,7 @@ load_pradeep2020 <- function(
   tmp_pradeep2020 <- httk::pradeep2020
   # check whether there is any information on specific chemicals to include
   if(!is.null(chem_include)){
-    # check that they are CAS number format
-    chem_include_check <- all(cas_id_check(chem_include)==TRUE)
-    if(chem_include_check == FALSE){
-      stop("At least one chemical ID in `chem_include` does not follow the standard CAS/CASRN format.")
-    }
-    ## CHECKS ##
-    # obtain any chemicals that are not in the CASRN list
-    out_CAS_list <- which(!(chem_include%in%tmp_pradeep2020[,"CASRN"]))
-    if(length(out_CAS_list)==length(chem_include)){
-      # provide an error message if none of the chemical identifiers to include are
-      # available in the dataset
-      stop("None of the CAS/CASRN chemical identifiers provided are in `pradeep2020`.")
-    }
-    if(length(out_CAS_list)>0){
-      # provide a message listing the chemical identifiers that are not available
-      # in the dataset
-      cat("The following CAS/CASRN chemical identifiers are not in `pradeep2020`:\n\t",
-          paste0(chem_include[out_CAS_list],collapse = ", "))
-    }
-    # subset to the chemicals that are included in the dataset
-    tmp_pradeep2020 <- tmp_pradeep2020 %>% 
-      dplyr::filter(CASRN %in% chem_include)
+    tmp_pradeep2020 <- httk_chem_subset(tmp_pradeep2020,chem_include = chem_include)
   }
   
   cat(paste("Loading CLint and Fup predictions from Pradeep et al. (2020) for",
