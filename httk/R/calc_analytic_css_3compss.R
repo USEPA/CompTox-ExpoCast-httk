941dda837153381e6a406f567c29731726485c8e
Wambaugh <Wambaugh.John@epa.gov>
2025-03-28 15:49:05 -0400

    yesterday's function

--- httk/R/calc_analytic_css_3compss.R
+++ httk/R/calc_analytic_css_3compss.R
@@ -1,101 +1,63 @@
-#' Calculate the analytic steady state concentration for the three compartment
-#' steady-state model
+#'Calculate the analytic steady state concentration for the three compartment
+#'steady-state model
 #'
-#' This function calculates the steady state plasma or venous blood 
-#' concentrations as a result of constant oral infusion dosing. 
-#' The equation, initally used 
-#' for high throughput in vitro-in vivo extrapolation in
-#' \insertCite{rotroff2010incorporating}{httk} and later given in 
-#' \insertCite{wetmore2012integration}{httk}, assumes that the concentration 
-#' is the inverse of the total clearance, which is the sum of hepatic metabolism
-#' and renal filatrion:
-#' \deqn{C^{ss}_{plasma} = \frac{dose}{f_{up}*Q_{GFR}+Cl_{h}}}
-#' \deqn{C^{ss}_{blood} = R_{b:p}*C^{ss}_{plasma}}
-#'  where Q_GFR is the glomerular filtration
-#' rate in the kidney, Cl_h is the chemical-specific whole liver metabolism 
-#' clearance (scaled up from intrinsic clearance, which does not depend on flow),
-#' f_up is the chemical-specific fraction unbound in plasma, R_b:p is the 
-#' chemical specific ratio of concentrations in blood:plasma.
-#'
-#' This equation is a simplification of the steady-state plasma concentration
-#' in the three-comprtment model (see \code{\link{solve_3comp}}), neglecting a
-#' higher order term that causes this Css to be higher for very rapidly cleared
-#' chemicals.
+#'This function calculates the analytic steady state plasma or venous blood 
+#'concentrations as a result of infusion dosing.
 #'
 #'@param chem.name Either the chemical name, CAS number, or the parameters must 
 #' be specified.
-#'
 #'@param chem.cas Either the chemical name, CAS number, or the parameters must 
 #' be specified.
-#'
 #' @param dtxsid EPA's 'DSSTox Structure ID (\url{https://comptox.epa.gov/dashboard})   
 #' the chemical must be identified by either CAS, name, or DTXSIDs
-#'
 #'@param parameters Chemical parameters from parameterize_pbtk (for model = 
 #' 'pbtk'), parameterize_3comp (for model = '3compartment), 
 #' parameterize_1comp(for model = '1compartment') or parameterize_steadystate 
 #' (for model = '3compartmentss'), overrides chem.name and chem.cas.
-#'
 #'@param hourly.dose Hourly dose rate mg/kg BW/h.
-#'
 #'@param concentration Desired concentration type, 'blood' or default 'plasma'.
-#'
 #'@param suppress.messages Whether or not the output message is suppressed.
-#'
 #'@param recalc.blood2plasma Recalculates the ratio of the amount of chemical 
 #' in the blood to plasma using the input parameters. Use this if you have 
 #' 'altered hematocrit, Funbound.plasma, or Krbc2pu.
-#'
 #'@param tissue Desired tissue concentration (defaults to whole body 
 #'concentration.)
-#'
 #'@param restrictive.clearance If TRUE (default), then only the fraction of
 #' chemical not bound to protein is available for metabolism in the liver. If 
 #' FALSE, then all chemical in the liver is metabolized (faster metabolism due
 #' to rapid off-binding). 
-#'
 #'@param bioactive.free.invivo If FALSE (default), then the total concentration is treated
 #' as bioactive in vivo. If TRUE, the the unbound (free) plasma concentration is treated as 
 #' bioactive in vivo. Only works with tissue = NULL in current implementation.
 #' 
-#' @param dosing List of dosing metrics used in simulation, which includes
-#' the namesake entries of a model's associated dosing.params. For steady-state
-#' calculations this is likely to be either "daily.dose" for oral exposures or
-#' "Cinhaled" for inhalation.
-#'
-#' @param dose.units The units associated with the dose received.
-#' 
 #' @param Caco2.options A list of options to use when working with Caco2 apical to
-#' basolateral data \code{Caco2.Pab}, default is Caco2.options = list(Caco2.Pab.default = 1.6,
-#' Caco2.Fabs = TRUE, Caco2.Fgut = TRUE, overwrite.invivo = FALSE, keepit100 = FALSE). Caco2.Pab.default sets the default value for 
+#' basolateral data \code{Caco2.Pab}, default is Caco2.options = list(Caco2.default = 2,
+#' Caco2.Fabs = TRUE, Caco2.Fgut = TRUE, overwrite.invivo = FALSE, keepit100 = FALSE). Caco2.default sets the default value for 
 #' Caco2.Pab if Caco2.Pab is unavailable. Caco2.Fabs = TRUE uses Caco2.Pab to calculate
 #' fabs.oral, otherwise fabs.oral = \code{Fabs}. Caco2.Fgut = TRUE uses Caco2.Pab to calculate 
 #' fgut.oral, otherwise fgut.oral = \code{Fgut}. overwrite.invivo = TRUE overwrites Fabs and Fgut in vivo values from literature with 
 #' Caco2 derived values if available. keepit100 = TRUE overwrites Fabs and Fgut with 1 (i.e. 100 percent) regardless of other settings.
-#' See \code{\link{get_fbio}} for further details.
 #' 
 #'@param ... Additional parameters passed to parameterize function if 
 #'parameters is NULL.
 #'  
-#' @return Steady state plasma concentration in mg/L units
+#'@return Steady state plasma concentration in mg/L units
 #'
 #' @seealso \code{\link{calc_analytic_css}}
 #'
 #' @seealso \code{\link{parameterize_steadystate}}
 #'
-#' @author Robert Pearce and John Wambaugh
+#'@author Robert Pearce and John Wambaugh
 #'
 #' @references 
-#' \insertAllCited{}
+#' \insertRef{pearce2017httk}{httk}
 #'
-#' @keywords 3compss
+#'@keywords 3compss
 calc_analytic_css_3compss <- function(chem.name=NULL,
                                    chem.cas = NULL,
                                    dtxsid = NULL,
                                    parameters=NULL,
-                                   dosing=list(daily.dose=1),
-                                   hourly.dose = NULL,
-                                   dose.units = "mg",
+                                   hourly.dose=1/24,
                                    concentration='plasma',
                                    suppress.messages=FALSE,
                                    recalc.blood2plasma=FALSE,
@@ -105,17 +67,9 @@ calc_analytic_css_3compss <- function(chem.name=NULL,
                                    Caco2.options = list(),
                                    ...)
 {
-  if (!is.null(hourly.dose))
-  {
-     warning("calc_analytic_css_3compss deprecated argument hourly.dose replaced with new argument dose, value given assigned to dose")
-     dosing <- list(daily.dose = 24*hourly.dose)
-  }
-  
-# Load from modelinfo file:
-  THIS.MODEL <- "3compartmentss"
-  param.names <- model.list[[THIS.MODEL]]$param.names
+
+  param.names.3compss <- model.list[["3compartmentss"]]$param.names
   param.names.schmitt <- model.list[["schmitt"]]$param.names
-  parameterize_function <- model.list[[THIS.MODEL]]$parameterize.func
     
 # We need to describe the chemical to be simulated one way or another:
   if (is.null(chem.cas) & 
@@ -155,21 +109,20 @@ calc_analytic_css_3compss <- function(chem.name=NULL,
       warning("Argument recalc.blood2plasma=TRUE ignored because parameters is NULL.")
     }
     
-    parameters <- do.call(what=parameterize_function, 
-                          args=purrr::compact(c(
-                            list(chem.cas=chem.cas,
-                                 chem.name=chem.name,
-                                 suppress.messages=suppress.messages,
-                                 Caco2.options = Caco2.options,
-                                 restrictive.clearance = restrictive.clearance
-                                 ),
-                            ...)))
+    parameters <- parameterize_steadystate(
+                                    chem.cas=chem.cas,
+                                    chem.name=chem.name,
+                                    dtxsid=dtxsid,
+                                    suppress.messages=suppress.messages,
+                                    restrictive.clearance=restrictive.clearance,
+                                    Caco2.options = Caco2.options,
+                                    ...)
 
   } else {
-    if (!all(param.names %in% names(parameters)))
+    if (!all(param.names.3compss %in% names(parameters)))
     {
       stop(paste("Missing parameters:",
-                 paste(param.names[which(!param.names %in% names(parameters))],
+                 paste(param.names.3compss[which(!param.names.3compss %in% names(parameters))],
                    collapse=', '),
                  ".  Use parameters from parameterize_steadystate."))
     }
@@ -186,49 +139,27 @@ calc_analytic_css_3compss <- function(chem.name=NULL,
                                                    hematocrit=parameters$hematocrit)
   }
 
-  BW <- parameters$BW
-  
-  # Dose rate:
-  hourly.dose <- dosing[["daily.dose"]] /
-                   24 /
-                   BW *
-                   convert_units(MW = parameters[["MW"]],
-                                 dose.units,
-                                 "mg") # mg/kg/h
-
   Fup <- parameters$Funbound.plasma
   Rb2p <- parameters$Rblood2plasma 
-
+  BW <- parameters$BW
   # Total blood flow (gut plus arterial) into liver:
-  Qtotalliver <- parameters$Qtotal.liverc/BW^0.25 # L / h / kg BW
+  Qtotalliver <- parameters$Qtotal.liverc/BW^0.25 #L/h/kg BW
 
-  # Scale glomerular filtration rate (for kidney elimination) to per kg BW:
-  Qgfr <- parameters$Qgfrc/BW^0.25 # L / h / kg BW
+# Scale up from in vitro Clint to a whole liver clearance:
+  cl <- calc_hep_clearance(parameters=parameters,
+          hepatic.model='unscaled',
+          suppress.messages=TRUE)#L/h/kg body weight
+  if (!restrictive.clearance) cl <- cl*Fup
 
-  # Scale up from in vitro Clint to a whole liver clearance:
-  Clhep <- calc_hep_clearance(parameters=parameters,
-          hepatic.model="well-stirred",
-          restrictive.clearance = restrictive.clearance,
-          suppress.messages=TRUE) # L / h / kg BW
-
-  # Oral bioavailability:
-  Fabsgut <- parameters$Fabsgut
-  Fhep <- parameters$hepatic.bioavailability
-
-
-# Calculate steady-state plasma Css. With the well-stirred calculation (above)
-# this is the same equation as Wetmore et al. (2012) page 160 or 
-# Pearce et al. (2017) equation section 2.2:
-
-   Css <- hourly.dose * # Oral dose rate mg/kg/h
-          Fabsgut * # Fraction of dose absorbed from gut (in vivo or Caco-2)
-          Fhep / # Fraction of dose that escapes first-pass hepatic metabolism
-          (
-            Qgfr * Fup + # Glomerular filtration to proximal tubules (kidney)
-            Clhep # Well-stirred hepatic metabolism (liver)
-          )
-
-# Css has units of mg / L
+# Calculate steady-state blood Css, Pearce et al. (2017) equation section 2.2:
+   Css_blood <- parameters$Fabsgut * 
+    parameters$hepatic.bioavailability *
+    hourly.dose / (
+    parameters$Qgfrc/BW^0.25 * Fup + 
+    Qtotalliver*Fup*cl /
+    (Qtotalliver + Fup*cl/Rb2p))
+  # Convert from blood to plasma:
+  Css <- Css_blood/Rb2p
     
 # Check to see if a specific tissue was asked for:
   if (!is.null(tissue))
@@ -276,8 +207,8 @@ calc_analytic_css_3compss <- function(chem.name=NULL,
     
     if (tolower(concentration)=='blood')
     {
-  # Convert from blood to plasma:
-        Css <-Css*Rb2p
+      Css <- Css * Rb2p
+      
     }else if(bioactive.free.invivo == TRUE & tolower(concentration) == 'plasma'){
       
       Css <- Css * parameters[['Funbound.plasma']]
@@ -286,6 +217,7 @@ calc_analytic_css_3compss <- function(chem.name=NULL,
   }
   return(Css)
 }
+
 
 # Add some parameters to the output from parameterize_steady_state so that
 # predict_partitioning_schmitt can run without reparameterizing
