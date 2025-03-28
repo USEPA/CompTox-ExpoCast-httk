f6aab390740c1abeac00616e835a9cc036eb926e
Wambaugh <Wambaugh.John@epa.gov>
2025-03-28 12:16:52 -0400

    more bugs from check --as-cran

--- httk/R/modelinfo_fetal_pbtk.R
+++ httk/R/modelinfo_fetal_pbtk.R
@@ -617,10 +617,16 @@ model.list[[THIS.MODEL]]$routes <- list(
 # desolve events can take the values "add" to add dose C1 <- C1 + dose,
 # "replace" to change the value C1 <- dose
 # or "multiply" to change the value to C1 <- C1*dose
-    "dose.type" = "add"),
+    "dose.type" = "add",
+"dosing.params" = c("daily.dose",
+                    "initial.dose",
+                    "doses.per.day",
+                    "dosing.matrix")),
   "iv" = list(
     "entry.compartment" = "Aven",
-    "dose.type" = "add")
+    "dose.type" = "add",
+    "dosing.params" = c("initial.dose",
+                       "dosing.matrix"))
   )
 
 # ORDERED LIST of state variables (must match Model variables: 
