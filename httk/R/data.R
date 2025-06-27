f5046e5efcee86666e86d00b9f1cc782f427a9d2
Wambaugh <Wambaugh.John@epa.gov>
2025-06-27 10:40:24 -0400

    Making vignette behave for R CMD build

--- httk/R/data.R
+++ httk/R/data.R
@@ -267,29 +267,6 @@
 #' \insertRef{honda2019using}{httk}
 "well_param"
 
-#' Armitage et al. (2014) Model Inputs from Honda et al. (2019)
-#'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{MP}{}
-#'   \item{MW}{}
-#'   \item{casrn}{}
-#'   \item{compound_name}{}
-#'   \item{gkaw}{}
-#'   \item{gkow}{}
-#'   \item{gswat}{}
-#' }
-#' @source \url{https://www.diamondse.info/}
-#'
-#'@keywords data
-#'
-#'@author Greg Honda
-#' @references 
-#' \insertRef{armitage2014application}{httk} 
-#'
-#' \insertRef{honda2019using}{httk}
-"armitage_input"
-
 #' DRUGS|NORMAN: Pharmaceutical List with EU, Swiss, US Consumption Data
 #'
 #' SWISSPHARMA is a list of pharmaceuticals with consumption data from
